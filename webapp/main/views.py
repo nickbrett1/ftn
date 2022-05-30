ba2fc169555ed964478a6a6fde1ca14c6ce93577
Nick Brett <nick.brett1@gmail.com>
2022-05-30 12:19:08 -0400

    Add support for Azure Key Vault

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from decouple import config
 
 def login(request):
   return render(request, 'main/login.html', context=None)
--- webapp/webapp/settings/base.py
+++ webapp/webapp/settings/base.py
@@ -15,17 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i1c1qhk=9m&@5ln-j_)!xc=c45x@b3*$drfkj+#sy2!-pj8#t('
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['nickbrett-bem.azurewebsites.net', 'localhost']
 
 
 # Application definition
