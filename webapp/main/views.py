cc836ddad5d83ea4982d668ddd14162c38ecffe3
Nick Brett <nick.brett1@gmail.com>
2022-05-30 12:36:26 -0400

    Test for Azure Credentials

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from decouple import config
 
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+
 def login(request):
-  return render(request, 'main/login.html', context=None)
+  try:
+    secret = SecretClient(vault_url = "https://nickbrett-bem-azvault.vault.azure.net/", 
+                            credential = DefaultAzureCredential()).get_secret('djangosecret')
+    return render(request, 'main/login.html', context= { 'secret' : secret })
+  except:
+    return render(request, 'main/login.html', context= { 'secret' : None })
