d6e9684bdc5684b6642bba7099ef52110d6e5f75
Nick Brett <nick.brett1@gmail.com>
2022-06-05 16:35:18 -0400

    Fix static_root

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -146,14 +146,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
 WEBPACK_LOADER = {
   'DEFAULT': {
+    'CACHE': not DEBUG,
     'STATS_FILE': os.path.join(BASE_DIR,'webpack-stats.json'),
     'BUNDLE_DIR_NAME' : 'webpack_bundles/',
+    'POLL_INTERVAL': 0.1,
+    'TIMEOUT': None,
+    'IGNORE': [r'.+\.hot-update.js', r'.+\.map']
   }
 }
 
