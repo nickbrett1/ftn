bc05660b29c6142eaaada5ca0caed88e0821f3a5
Nick Brett <nick.brett1@gmail.com>
2022-06-19 22:52:03 +0000

    remove csrf exempt

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True)))
+    path('graphql', GraphQLView.as_view(graphiql=True))
 ]
