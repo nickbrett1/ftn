d7e581f1703ce6364740ada9fb68c22e9c2f88fa
Nick Brett <nick.brett1@gmail.com>
2022-06-19 02:01:39 +0000

    migrate database before gunicorn

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 # startup application
 
-gunicorn --bind=0.0.0.0 --timeout 600 --chdir webapp webapp.wsgi --access-logfile '-' --error-logfile '-'
 python webapp/manage.py migrate
+gunicorn --bind=0.0.0.0 --timeout 600 --chdir webapp webapp.wsgi --access-logfile '-' --error-logfile '-'
