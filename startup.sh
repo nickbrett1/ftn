0f4f69ead069055061b6fecc4df7248a34b5b75e
Nick Brett <nick.brett1@gmail.com>
2022-06-19 03:00:59 +0000

    superuser create on deploy

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 # startup application
 
 python webapp/manage.py migrate
+python webapp/manage.py createsuperuser --noinput
 gunicorn --bind=0.0.0.0 --timeout 600 --chdir webapp webapp.wsgi --access-logfile '-' --error-logfile '-'
