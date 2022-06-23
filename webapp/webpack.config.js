a17a431f65dd001b54830e0b0e4cc591015396e9
Nick Brett <nick.brett1@gmail.com>
2022-06-23 21:14:45 +0000

    lint

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   mode: 'development',
   output: {
     path: path.resolve('./assets/webpack_bundles/'),
-    filename: '[name]-[contenthash].js'
+    filename: '[name]-[contenthash].js',
   },
   plugins: [
     new BundleTracker({ filename: './webpack-stats.json' }),
@@ -20,8 +20,8 @@ module.exports = {
       type: 'https',
       options: {
         key: '../localhost+2-key.pem',
-        cert: '../localhost+2.pem'
-      }
+        cert: '../localhost+2.pem',
+      },
     },
     devMiddleware: {
       index: true,
