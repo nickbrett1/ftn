0e9e9fcd1f7e01f9eb6682ea9841776a32236f16
Nick Brett <nick.brett1@gmail.com>
2022-07-24 01:38:24 +0000

    With image

--- webapp/main/src/App.jsx
+++ webapp/main/src/App.jsx
@@ -3,17 +3,45 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Login from './Login';
 
-function App() {
-  return (
+function LandingFrameMessage() {
+  const style = {
+    margin: "auto",
+    padding: "10% 35% 10% 15%",
+    color: "white"
+  }
+
+  return <div style={style}>
+    <div style={{"fontSize": "96px"}}>
+      British Empire Management
+    </div>
+    <div style={{"fontSize": "36px"}}>
+      The sun never sets
+    </div>
+    <br />
     <GoogleOAuthProvider>
-      <h1>Login to British Empire Management!!</h1>
       <Login />
     </GoogleOAuthProvider>
-  );
+  </div>
 }
 
-export default App;
+
+function LandingFrame() {
+  const style = {
+    'backgroundImage': 'url(' + require('./images/unionjack.jpg') + ')',
+    'backgroundRepeat': 'no-repeat',
+    'backgroundSize': 'cover',
+    'position': 'absolute',
+    'height': '100%',
+    'width': '100%'
+  }
+
+  return <div style={style}>
+    <LandingFrameMessage />
+  </div>
+}
+
+export default LandingFrame;
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(<App />);
+root.render(<LandingFrame />);
