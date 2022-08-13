1c9ed9ec558080f4c1dace8668961faeba3fb6e9
Nick Brett <nick.brett1@gmail.com>
2022-08-13 20:24:38 +0000

    Fix formating and use Grid for display

--- webapp/main/src/App.jsx
+++ webapp/main/src/App.jsx
@@ -9,32 +9,29 @@ import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Login from './Login';
-import { Typography, Box } from '@mui/material';
+import { Typography, Grid } from '@mui/material';
 
 function LandingFrameMessage() {
-  const style = {
-    margin: 'auto',
-    padding: '10% 10% 10% 10%',
-    color: 'white'
-  }
-
   return (
-    <Box maxWidth='sm' padding='30px'>
-      <Typography variant='h1'>
-        British Empire Management
-      </Typography>
-      <Typography variant='h3'>
-        The Sun Never Sets
-      </Typography>
-      
-      <GoogleOAuthProvider clientId='263846603498-57v6mk1hacurssur6atn1tiplsnv4j18.apps.googleusercontent.com'>
-        <Login />
-      </GoogleOAuthProvider>
-    </Box>
+    <Grid container padding='30px' spacing={1}>
+      <Grid item xs={12}>
+        <Typography variant='h1'>
+          British Empire Management
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant='h3'>
+          The Sun Never Sets
+        </Typography>
+      </Grid>
+      <Grid item xs={2}>
+        <GoogleOAuthProvider clientId='263846603498-57v6mk1hacurssur6atn1tiplsnv4j18.apps.googleusercontent.com'>
+          <Login />
+        </GoogleOAuthProvider>
+      </Grid>
+    </Grid>
   )
 }
-
-
 
 function LandingFrame() {
   const style = {
@@ -49,8 +46,18 @@ function LandingFrame() {
   const darkTheme = createTheme({
     palette: {
       mode: 'dark',
+      primary: {
+        main: '#3f51b5',
+      },
+      secondary: {
+        main: '#f50057',
+      },
     },
-  });
+    typography: {
+      useNextVariants: true
+    }
+  }
+  );
 
   return (
     <ThemeProvider theme={darkTheme}>
