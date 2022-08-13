73358b9839455868c7b9244299fd0aa054f23552
Nick Brett <nick.brett1@gmail.com>
2022-08-13 00:12:20 +0000

    Add material support, css loading and theming

--- webapp/main/src/App.jsx
+++ webapp/main/src/App.jsx
@@ -1,6 +1,13 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Login from './Login';
 
 function LandingFrameMessage() {
@@ -14,16 +21,21 @@ function LandingFrameMessage() {
     <div style={{'fontSize': '6em'}}>
       British Empire Management
     </div>
-    <div style={{'fontSize': '2em'}}>
-      The sun never sets
+    <div style={{'fontSize': '3em', 'marginBottom' : '15px'}}>
+      The Sun Never Sets
     </div>
     
-    <GoogleOAuthProvider>
+    <GoogleOAuthProvider clientId='263846603498-57v6mk1hacurssur6atn1tiplsnv4j18.apps.googleusercontent.com'>
       <Login />
     </GoogleOAuthProvider>
   </div>
 }
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function LandingFrame() {
   const style = {
@@ -35,9 +47,13 @@ function LandingFrame() {
     'width': '100%',
   }
 
-  return <div style={style}>
-    <LandingFrameMessage />
-  </div>
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      <div style={style}>
+      <LandingFrameMessage />
+      </div>
+    </ThemeProvider>)
 }
 
 export default LandingFrame;
