8e1c0eb3b9ac0f5294f58e7a4d6007f20d345000
Nick Brett <nick.brett1@gmail.com>
2022-08-28 23:26:38 +0000

    Add sentry org to webpack config

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -46,6 +46,7 @@ module.exports = {
       },
     }),
     new SentryWebpackPlugin({
+      organization: 'nick-brett',
       include: path.resolve('./assets/webpack_bundles/'),
       ignoreFile: '.sentrycliignore',
       ignore: ['node_modules', 'webpack.config.js'],
