d71d3b8c266b96c0219b88b4fc12c4a7fc8e0afa
Nick Brett <nick.brett1@gmail.com>
2022-08-31 01:40:23 +0000

    Breakup webpack configs

---
+++ webapp/webpack.common.js
@@ -0,0 +1,92 @@
+const BundleTracker = require('webpack-bundle-tracker');
+const WebpackFavicons = require('webpack-favicons');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ProgressPlugin = require('progress-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './main/src/App.jsx',
+  output: {
+    path: path.resolve('./assets/webpack_bundles/'),
+    publicPath: '/static/webpack_bundles/',
+    filename: '[name]-[contenthash].js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ProgressPlugin(true),
+    new BundleTracker({ filename: './webpack-stats.json' }),
+
+    new WebpackFavicons({
+      src: './main/src/images/flag.svg',
+      appName: 'British Empire Management',
+      appShortName: 'BEM',
+      appDescription: 'British Empire Management',
+      scope: '/',
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        favicons: true,
+        windows: true,
+        yandex: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './main/templates/main/index.html',
+      title: 'British Empire Management',
+      filename: 'index.html',
+      publicPath: '/static/webpack_bundles',
+      templateParameters: {
+        debug: process.env.DEBUG,
+      },
+      meta: {
+        charset: 'utf-8',
+        viewport: 'width=device-width, initial-scale=1',
+      },
+    }),
+    new SentryWebpackPlugin({
+      org: 'nick-brett',
+      include: path.resolve('./assets/webpack_bundles/'),
+      ignoreFile: '.sentrycliignore',
+      ignore: ['node_modules', 'webpack.config.js'],
+      configFile: 'sentry.properties',
+      project: 'bem-frontend',
+      urlPrefix: '~/static/webpack_bundles',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: 'css-loader',
+      },
+      {
+        test: /\.ico$/,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][ext][query]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '...'],
+  },
+};
---
+++ webapp/webpack.dev.js
@@ -0,0 +1,35 @@
+const { resolve } = require('path');
+const { merge } = require('webpack-merge');
+const { existsSync } = require('fs');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-source-map',
+  devServer: {
+    server: {
+      type: 'https',
+      options: {
+        key: existsSync('../localhost+2-key.pem')
+          ? '../localhost+2-key.pem'
+          : '/home/vscode/.local/lib/python3.8/site-packages/sslserver/certs/development.key',
+        cert: existsSync('../localhost+2.pem')
+          ? '../localhost+2.pem'
+          : '/home/vscode/.local/lib/python3.8/site-packages/sslserver/certs/development.crt',
+      },
+    },
+    client: {
+      overlay: true,
+    },
+    devMiddleware: {
+      index: true,
+      mimeTypes: { phtml: 'text/html' },
+      publicPath: resolve('./assets/webpack_bundles/'),
+      serverSideRender: true,
+      writeToDisk: true,
+    },
+  },
+  plugins: [new BundleAnalyzerPlugin()],
+});
