773ea28092c7016ab054594b866f8da2f425e5a0
Nick Brett <nick.brett1@gmail.com>
2022-09-07 01:42:57 +0000

    Remove python dependency

--- startup.sh
+++
@@ -1,5 +0,0 @@
-# startup application
-
-python webapp/manage.py migrate
-python webapp/manage.py createsuperuser --noinput
-gunicorn --bind=0.0.0.0 --timeout 600 --chdir webapp webapp.wsgi --access-logfile '-' --error-logfile '-'
