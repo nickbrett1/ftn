b6fdb162dd5e8afad2aa44f73490d8a3cc039627
Nick Brett <nick.brett1@gmail.com>
2022-09-09 01:37:05 +0000

    Get webpack stats working

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'implicit-arrow-linebreak': ['error', 'below'],
   },
 };
