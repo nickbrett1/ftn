18d22ccbbce3d98acda5e230f2209ec781ebea01
Nick Brett <nick.brett1@gmail.com>
2022-09-14 01:36:57 +0000

    Try setting specific url

--- webapp/.lighthouserc.js
+++ webapp/.lighthouserc.js
@@ -2,6 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './assets/webpack_bundles',
+      url: 'https://localhost',
     },
     upload: {
       target: 'temporary-public-storage',
