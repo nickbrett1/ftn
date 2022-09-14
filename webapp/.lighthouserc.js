a1b172cda47430f942560a720623dfb04615645d
Nick Brett <nick.brett1@gmail.com>
2022-09-14 01:39:30 +0000

    Try to force port

--- webapp/.lighthouserc.js
+++ webapp/.lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './assets/webpack_bundles',
-      url: 'https://localhost',
+      url: 'http://localhost:46657',
     },
     upload: {
       target: 'temporary-public-storage',
