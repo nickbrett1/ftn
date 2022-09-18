b3aebdfb155604ec1bf6d9c0459eba48bbfba85f
Nick Brett <nick.brett1@gmail.com>
2022-09-18 01:06:29 +0000

    Migrate to nextjs

---
+++ webapp/.next/server/pages/_app.js
@@ -0,0 +1,186 @@
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 484:
+/***/ (() => {
+
+var _global = (typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {}); _global.SENTRY_RELEASE={id:"SKJ_I6npCnjx16Vr40v84"};
+
+/***/ }),
+
+/***/ 932:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+var __webpack_unused_export__;
+
+__webpack_unused_export__ = ({
+    value: true
+});
+exports.Z = _asyncToGenerator;
+function _asyncToGenerator(fn) {
+    return function() {
+        var self = this, args = arguments;
+        return new Promise(function(resolve, reject) {
+            var gen = fn.apply(self, args);
+            function _next(value) {
+                asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+            }
+            function _throw(err) {
+                asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+            }
+            _next(undefined);
+        });
+    };
+}
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+    try {
+        var info = gen[key](arg);
+        var value = info.value;
+    } catch (error) {
+        reject(error);
+        return;
+    }
+    if (info.done) {
+        resolve(value);
+    } else {
+        Promise.resolve(value).then(_next, _throw);
+    }
+}
+
+
+/***/ }),
+
+/***/ 648:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+var __webpack_unused_export__;
+
+__webpack_unused_export__ = ({
+    value: true
+});
+exports.Z = _interopRequireDefault;
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}
+
+
+/***/ }),
+
+/***/ 107:
+/***/ ((__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony import */ var _sentry_nextjs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(97);
+/* harmony import */ var _sentry_nextjs__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_sentry_nextjs__WEBPACK_IMPORTED_MODULE_0__);
+global.__rewriteFramesDistDir__ = ".next";
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+_sentry_nextjs__WEBPACK_IMPORTED_MODULE_0__.init({
+    dsn: SENTRY_DSN || "https://2becbe2880ce41ed8198fd63c2cd490f@o1381755.ingest.sentry.io/6695436",
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1.0
+});
+
+
+/***/ }),
+
+/***/ 29:
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({
+    value: true
+}));
+Object.defineProperty(exports, "AppInitialProps", ({
+    enumerable: true,
+    get: function() {
+        return _utils.AppInitialProps;
+    }
+}));
+Object.defineProperty(exports, "NextWebVitalsMetric", ({
+    enumerable: true,
+    get: function() {
+        return _utils.NextWebVitalsMetric;
+    }
+}));
+exports["default"] = void 0;
+var _async_to_generator = (__webpack_require__(932)/* ["default"] */ .Z);
+var _interop_require_default = (__webpack_require__(648)/* ["default"] */ .Z);
+var _react = _interop_require_default(__webpack_require__(689));
+var _utils = __webpack_require__(232);
+function appGetInitialProps(_) {
+    return _appGetInitialProps.apply(this, arguments);
+}
+function _appGetInitialProps() {
+    _appGetInitialProps = /**
+ * `App` component is used for initialize of pages. It allows for overwriting and full control of the `page` initialization.
+ * This allows for keeping state between navigation, custom error handling, injecting additional data.
+ */ _async_to_generator(function*({ Component , ctx  }) {
+        const pageProps = yield (0, _utils).loadGetInitialProps(Component, ctx);
+        return {
+            pageProps
+        };
+    });
+    return _appGetInitialProps.apply(this, arguments);
+}
+var _Component;
+class App extends (_Component = _react.default.Component) {
+    render() {
+        const { Component , pageProps  } = this.props;
+        return /*#__PURE__*/ _react.default.createElement(Component, Object.assign({}, pageProps));
+    }
+}
+App.origGetInitialProps = appGetInitialProps;
+App.getInitialProps = appGetInitialProps;
+exports["default"] = App; //# sourceMappingURL=_app.js.map
+
+
+/***/ }),
+
+/***/ 97:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@sentry/nextjs");
+
+/***/ }),
+
+/***/ 232:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(484), __webpack_exec__(107), __webpack_exec__(29));
+module.exports = __webpack_exports__;
+
+})();
+//# sourceMappingURL=_app.js.map
---
+++ webapp/components/Login.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useGoogleLogin } from '@react-oauth/google';
+import SvgIcon from '@mui/material/SvgIcon';
+import Button from '@mui/material/Button';
+import jwtDecode from 'jwt-decode';
+
+export default function Login() {
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      jwtDecode(codeResponse);
+    },
+    onError: () => {},
+    flow: 'auth-code',
+  });
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      size="large"
+      startIcon={
+        <SvgIcon>
+          <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+        </SvgIcon>
+      }
+      onClick={() => {
+        login();
+        throw new Error('Sentry Frontend Error');
+      }}
+    >
+      Login
+    </Button>
+  );
+}
