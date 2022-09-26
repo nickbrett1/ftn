50e287fc8a6291b339bea52aa59f28f4b4be9192
Nick Brett <nick.brett1@gmail.com>
2022-09-26 00:51:40 +0000

    Try again with sentry setup in pages middleware

--- webapp/functions/_middleware.js
+++ webapp/functions/_middleware.js
@@ -1,6 +1,13 @@
 import sentryPlugin from '@cloudflare/pages-plugin-sentry';
 import createSentryConfig from '../sentry/sentry';
 
-export default async function onRequest() {
-  return sentryPlugin(createSentryConfig());
-}
+// export const onRequestHead = [headerSetup];
+
+const hello = async ({ next }) => {
+  const response = await next();
+  response.headers.set('X-Hello', 'Hello from functions Middleware!');
+  return response;
+};
+
+export const onRequest = [sentryPlugin(createSentryConfig()), hello];
+export default {};
