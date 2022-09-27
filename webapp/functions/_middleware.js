3771aa157882cfae062f3fa9aeb738b2fb22de3e
Nick Brett <nick.brett1@gmail.com>
2022-09-27 01:24:54 +0000

    Split sentry backend logging to new project

--- webapp/functions/_middleware.js
+++ webapp/functions/_middleware.js
@@ -1,5 +1,4 @@
 import sentryPlugin from '@cloudflare/pages-plugin-sentry';
-import createSentryConfig from '../sentry/sentry';
 
 // export const onRequestHead = [headerSetup];
 
@@ -9,5 +8,22 @@ const hello = async ({ next }) => {
   return response;
 };
 
-export const onRequest = [sentryPlugin(createSentryConfig()), hello];
+export const onRequest = [
+  sentryPlugin({
+    dsn: 'https://09dfa4a627e04814b584374fe183c61d@o1381755.ingest.sentry.io/6778159',
+
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1.0,
+    attachStacktrace: true,
+
+    allowedHeaders: ['user-agent'],
+    allowedSearchParams: /(.*)/,
+
+    // ...
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
+  }),
+  hello,
+];
 export default {};
