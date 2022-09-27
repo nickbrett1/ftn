4b04a844e2ca3adca6eb88dd540ee9a6f9ab0b4d
Nick Brett <nick.brett1@gmail.com>
2022-09-27 01:24:54 +0000

    Split sentry backend logging to new project

--- webapp/sentry.client.config.js
+++ webapp/sentry.client.config.js
@@ -3,8 +3,18 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
-import createSentryConfig from './sentry/sentry';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init(createSentryConfig(SENTRY_DSN));
+Sentry.init({
+  dsn:
+    SENTRY_DSN ||
+    'https://2becbe2880ce41ed8198fd63c2cd490f@o1381755.ingest.sentry.io/6695436',
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  attachStacktrace: true,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
--- webapp/sentry.server.config.js
+++ webapp/sentry.server.config.js
@@ -7,7 +7,9 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://2becbe2880ce41ed8198fd63c2cd490f@o1381755.ingest.sentry.io/6695436',
+  dsn:
+    SENTRY_DSN ||
+    'https://2becbe2880ce41ed8198fd63c2cd490f@o1381755.ingest.sentry.io/6695436',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
