1b919d9bc0cb4a379d50c4a9a4cd2800e0480538
Nick Brett <nick.brett1@gmail.com>
2022-09-27 01:31:21 +0000

    Fix next.config.js

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -42,12 +42,17 @@ const sentryWebpackPluginOptions = {
   ignore: [],
 };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+const shouldAnalyzeBundles = process.env.ANALYZE === true;
 
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withBundleAnalyzer(
-  withSentryConfig(moduleExports, sentryWebpackPluginOptions)
-);
+if (shouldAnalyzeBundles) {
+  const withBundleAnalyzer = require('@next/bundle-analyzer');
+
+  module.exports = withBundleAnalyzer(
+    withSentryConfig(moduleExports, sentryWebpackPluginOptions)
+  );
+} else {
+  // Make sure adding Sentry options is the last code to run before exporting, to
+  // ensure that your source maps include changes from all other Webpack plugins
+
+  module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+}
