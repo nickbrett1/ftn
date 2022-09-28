cd377d0e93d9aa043dc2e921f21d899398d79f62
Nick Brett <nick.brett1@gmail.com>
2022-09-28 02:07:48 +0000

    Get worker bundle file into the right place

--- webapp/webpack.workers.config.js
+++ webapp/webpack.workers.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './workers/_worker.js',
   mode: 'production',
   output: {
-    path: path.resolve(__dirname, './out'),
-    filename: '_worker.js',
+    path: path.resolve(__dirname, './workers'),
+    filename: '_worker-bundle.js',
   },
 };
