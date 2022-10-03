42a2156bb5854da33bc4e199dca3ca1e6cdd38bd
Nick Brett <nick.brett1@gmail.com>
2022-10-03 22:39:40 +0000

    Try testing right env variable

--- webapp/workers/_worker.js
+++ webapp/workers/_worker.js
@@ -8,7 +8,7 @@ export default {
       request, // request is not included in 'context', so we set it here.
       allowedHeaders: ['user-agent'],
       allowedSearchParams: /(.*)/,
-      release: environment.production ? process.env.SENTRY_RELEASE : '',
+      release: environment.PRODUCTION ? process.env.SENTRY_RELEASE : '',
       rewriteFrames: {
         root: 'https://bemstudios.uk/workers/',
       },
