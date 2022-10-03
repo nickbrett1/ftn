5853898327cb07b850b47131b0cc671945f48a40
Nick Brett <nick.brett1@gmail.com>
2022-10-03 00:46:34 +0000

    Add nojs and better dev support in worker

---
+++ webapp/pages/nojs.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Head from 'next/head';
+import { Box, Typography } from '@mui/material';
+
+export default function NoJs() {
+  return (
+    <>
+      <Head>
+        <meta name="description" content="No JavaScript enabled" />
+        <title>JavaScript Not Enabled</title>
+      </Head>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '100vh',
+        }}
+      >
+        <Typography variant="h1">Javascript Not Enabled</Typography>
+      </Box>
+    </>
+  );
+}
