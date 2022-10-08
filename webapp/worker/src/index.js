ed9fc7f0f751b1caf4fab942857921086c19ae61
Nick Brett <nick.brett1@gmail.com>
2022-10-08 23:41:32 +0000

    Fix website

--- webapp/worker/src/index.js
+++ webapp/worker/src/index.js
@@ -35,8 +35,6 @@ export default {
       // Add a custom header with a value
       response.headers.append('x-workers-hello', 'I HAVE THE POWER');
 
-      throw new Error('Do redirects work?');
-
       return response;
     } catch (err) {
       sentry.captureException(err);
