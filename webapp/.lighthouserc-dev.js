14a56e21cac237d1fef7d523e9acbbb3ba541a3f
Nick Brett <nick.brett1@gmail.com>
2022-10-09 23:50:38 +0000

    Add wrangler to lhci

--- webapp/.lighthouserc-dev.js
+++ webapp/.lighthouserc-dev.js
@@ -12,14 +12,6 @@ module.exports = {
     },
     assert: {
       preset: 'lighthouse:recommended',
-      assertions: {
-        'service-worker': 'off',
-        // Awaiting support by Cloudflare for server side logic on Pages
-        // Maybe try header addition in workers?
-        // https://zhuhaow.me/deploy-full-nextjs-site-on-cloudflare-are-we-there-yet
-        // https://github.com/vercel/next.js/discussions/34179
-        'csp-xss': 'off',
-      },
     },
   },
 };
--- webapp/.lighthouserc.js
+++ webapp/.lighthouserc.js
@@ -1,9 +1,15 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'npm run start',
+      startServerCommand: 'npm run start && npm run wrangler-local',
       startServerReadyPattern: 'ready on',
       staticDistDir: './out',
+      url: [
+        'http://localhost:8787',
+        'http://localhost:8787/404',
+        'http://localhost:8787/500',
+        'http://localhost:8787/nojs',
+      ],
     },
     upload: {
       target: 'temporary-public-storage',
