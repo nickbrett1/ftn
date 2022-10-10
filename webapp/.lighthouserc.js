edac5f34e059a3d61326b9030faaa317344ee387
Nick Brett <nick.brett1@gmail.com>
2022-10-10 00:41:55 +0000

    Force lighthouse.js config

--- webapp/.lighthouserc.js
+++ webapp/.lighthouserc.js
@@ -3,13 +3,15 @@ module.exports = {
     collect: {
       startServerCommand: 'npm run start && npm run wrangler-local',
       startServerReadyPattern: 'ready on',
-      staticDistDir: './out',
       url: [
         'http://localhost:8787',
         'http://localhost:8787/404',
         'http://localhost:8787/500',
         'http://localhost:8787/nojs',
       ],
+      settings: {
+        cpuSlowdownMultiplier: 2.4,
+      },
     },
     upload: {
       target: 'temporary-public-storage',
