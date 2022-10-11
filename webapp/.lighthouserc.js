02917da2f4be811ab35e4b068a6e5f1cbfe0e10d
Nick Brett <nick.brett1@gmail.com>
2022-10-11 00:14:21 +0000

    Added support for staging environment for lhci

--- webapp/.lighthouserc.js
+++ webapp/.lighthouserc.js
@@ -1,11 +1,12 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: 'npm run wrangler-staging && npm run start',
+      startServerCommand: 'npm run staging',
       startServerReadyPattern: 'Starting a local server...',
       url: ['http://localhost:8787'],
       settings: {
         cpuSlowdownMultiplier: 2.4,
+        chromeFlags: '--no-sandbox',
       },
     },
     upload: {
