61184257bcff43e3e3e7ad00232df0ec021bf3ec
Nick Brett <nick.brett1@gmail.com>
2022-10-15 02:35:29 +0000

    Improved jest dev support

--- webapp/__tests__/index.test.jsx
+++ webapp/__tests__/index.test.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import LandingFrame from '../pages';
 import '@testing-library/jest-dom';
+import LandingFrame from '../pages';
 
 describe('LandingFrame', () => {
   it('renders the landing frame', () => {
