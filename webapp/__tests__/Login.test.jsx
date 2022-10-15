aa9534986d01b82dc7ac0a664e79c8f66c2e9227
Nick Brett <nick.brett1@gmail.com>
2022-10-15 20:23:40 +0000

    Add a Login button press test

---
+++ webapp/__tests__/Login.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Login from '../components/Login';
+
+describe('Login click', () => {
+  it('generates an onClick on Button press', () => {
+    const loginSpy = jest.fn();
+    render(<Login />);
+
+    const button = screen.getByText('Login');
+    button.onclick = loginSpy;
+    fireEvent.click(button);
+
+    expect(loginSpy).toBeCalled();
+  });
+});
