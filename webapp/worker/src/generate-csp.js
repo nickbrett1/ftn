226a54f17a3c32925b95b5cc9dd1b57a52c6ddca
Nick Brett <nick.brett1@gmail.com>
2022-10-15 21:13:09 +0000

    Update CSP for Google Login

--- webapp/worker/src/generate-csp.js
+++ webapp/worker/src/generate-csp.js
@@ -14,14 +14,18 @@ const generateCSP = () => {
   add('connect-src', 'https://fonts.googleapis.com');
   add('connect-src', 'https://fonts.gstatic.com');
   add('connect-src', 'https://cloudflareinsights.com');
+  add('connect-src', 'https://accounts.google.com/gsi/');
+  add('font-src', 'https://fonts.gstatic.com');
+  add('frame-src', 'https://accounts.google.com/gsi/');
+  add('img-src', `'self'`);
+  add('manifest-src', `'self'`);
+  add('script-src', `'unsafe-eval'`, { devOnly: true });
+  add('script-src', 'https://accounts.google.com/gsi/client');
   add('script-src-elem', `'self'`);
   add('script-src-elem', 'https://static.cloudflareinsights.com');
   add('style-src', `'unsafe-inline'`);
   add('style-src', 'https://fonts.googleapis.com');
-  add('font-src', 'https://fonts.gstatic.com');
-  add('manifest-src', `'self'`);
-  add('img-src', `'self'`);
-  add('script-src', `'unsafe-eval'`, { devOnly: true });
+  add('style-src', 'https://accounts.google.com/gsi/style');
   add('worker-src', `'self'`);
 
   return Object.entries(policy)
