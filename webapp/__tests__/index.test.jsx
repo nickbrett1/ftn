475c167bebe2f88e85c80e07191d2c7839d9eafc
Nick Brett <nick.brett1@gmail.com>
2022-10-16 01:34:00 +0000

    Add tests and more support for Google Login

--- webapp/__tests__/index.test.jsx
+++ webapp/__tests__/index.test.jsx
@@ -3,6 +3,8 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import LandingFrame from '../pages';
 
+jest.mock('@react-oauth/google');
+
 describe('LandingFrame', () => {
   it('renders the landing frame', () => {
     render(<LandingFrame />);
