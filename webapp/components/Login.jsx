89d378fcd96686a50c47c393599da58b3f65819d
Nick Brett <nick.brett1@gmail.com>
2022-10-21 00:51:27 +0000

    Add back google login support

--- webapp/components/Login.jsx
+++ webapp/components/Login.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SvgIcon from '@mui/material/SvgIcon';
 import Button from '@mui/material/Button';
 
 export default function Login() {
+  const [client, setClient] = useState(null);
+
   const login = () => {
+    if (client) {
+      client.requestAccessToken();
+      return;
+    }
+
     const script = document.createElement('script');
 
     script.src = 'https://accounts.google.com/gsi/client';
+    script.onload = () => {
+      const CLIENT_ID =
+        '263846603498-57v6mk1hacurssur6atn1tiplsnv4j18.apps.googleusercontent.com';
+
+      window.google.accounts.id.initialize({
+        client_id: CLIENT_ID,
+        callback: (response) => {
+          if (!response.credential || !response.clientId) {
+            throw new Error('Failed to initialize google sign in');
+          }
+        },
+      });
+
+      const clientRef = window.google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: `openid profile email`,
+        callback: (response) => {
+          if (response.error) {
+            throw new Error('Failed to initTokenClient', response.error);
+          }
+        },
+      });
+
+      setClient(clientRef);
+      clientRef.requestAccessToken();
+    };
+
+    script.onerror = () => {
+      throw new Error('Google gsi script failed to load');
+    };
+
     document.body.appendChild(script);
   };
 
