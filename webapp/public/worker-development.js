1b146886dec76f235eab38de3ad717566581af04
Nick Brett <nick.brett1@gmail.com>
2022-10-21 00:51:27 +0000

    Add back google login support

---
+++ webapp/public/worker-development.js
@@ -0,0 +1,6 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+// eslint-disable-next-line no-underscore-dangle,no-restricted-globals
+self.__WB_DISABLE_DEV_LOGS = true;
+/******/ })()
+;
