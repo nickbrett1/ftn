97655e1c49e09bf73a38f7602e68af0c7d0ab247
Nick Brett <nick.brett1@gmail.com>
2022-10-28 21:55:23 +0000

    Remove nojs lighthouse check

--- webapp/.lighthouserc.js
+++ webapp/.lighthouserc.js
@@ -3,11 +3,7 @@ module.exports = {
     collect: {
       startServerCommand: 'npm run staging',
       startServerReadyPattern: 'Starting a local server...',
-      url: [
-        'http://localhost:8787',
-        'http://localhost:8787/nojs',
-        'http://localhost:8787/home',
-      ],
+      url: ['http://localhost:8787', 'http://localhost:8787/home'],
       settings: {
         cpuSlowdownMultiplier: 2.4,
         chromeFlags: '--no-sandbox',
