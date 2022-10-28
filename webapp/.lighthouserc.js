e343183ebc208cab6c67d69b923572bfda2e3598
Nick Brett <nick.brett1@gmail.com>
2022-10-28 21:47:49 +0000

    Add lighthouse check for home

--- webapp/.lighthouserc.js
+++ webapp/.lighthouserc.js
@@ -3,7 +3,11 @@ module.exports = {
     collect: {
       startServerCommand: 'npm run staging',
       startServerReadyPattern: 'Starting a local server...',
-      url: ['http://localhost:8787', 'http://localhost:8787/nojs'],
+      url: [
+        'http://localhost:8787',
+        'http://localhost:8787/nojs',
+        'http://localhost:8787/home',
+      ],
       settings: {
         cpuSlowdownMultiplier: 2.4,
         chromeFlags: '--no-sandbox',
