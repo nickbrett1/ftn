b176552e47fbbec55bc9fe9e604bdf1c3d91e70e
Nick Brett <nick.brett1@gmail.com>
2022-11-02 00:40:44 +0000

    Fix tests

--- webapp/__tests__/Login.test.jsx
+++ webapp/__tests__/Login.test.jsx
@@ -3,8 +3,6 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Login from '../components/Login';
 
-jest.mock('@react-oauth/google');
-
 describe('Login click', () => {
   it('generates an onClick on Button press', () => {
     const loginSpy = jest.fn();
