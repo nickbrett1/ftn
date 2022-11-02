1519ea26ed449c4a4aac36792692cd0a6e74f4db
Nick Brett <nick.brett1@gmail.com>
2022-11-02 00:40:44 +0000

    Fix tests

--- webapp/__tests__/index.test.jsx
+++ webapp/__tests__/index.test.jsx
@@ -3,8 +3,6 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import LandingFrame from '../pages';
 
-jest.mock('@react-oauth/google');
-
 describe('LandingFrame', () => {
   it('renders the landing frame', () => {
     render(<LandingFrame />);
