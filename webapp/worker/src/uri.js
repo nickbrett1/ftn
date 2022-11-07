aa47f9f308c40b09865fc0b95c99f293c4cb847b
Nick Brett <nick.brett1@gmail.com>
2022-11-07 02:02:38 +0000

    Fix for lighthouse checks in circleci

--- webapp/worker/src/uri.js
+++ webapp/worker/src/uri.js
@@ -1,5 +1,8 @@
 const generateUriBase = (url, env) => {
-  if (env.SENTRY_ENVIRONMENT === 'development') {
+  if (
+    env.SENTRY_ENVIRONMENT === 'development' ||
+    env.SENTRY_ENVIRONMENT === 'staging'
+  ) {
     // Needed to forece the redirect to port 8787 for the worker, not 3000
     return 'http://localhost:8787';
   }
