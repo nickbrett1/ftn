bf04ff0fee81f5ce5d9e56129038c873ab59a3a0
Nick Brett <nick.brett1@gmail.com>
2022-11-21 01:51:47 +0000

    Added Login UI tests

--- webapp/__tests__/Login.test.jsx
+++ webapp/__tests__/Login.test.jsx
@@ -1,17 +1,60 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom';
 import Login from '../components/Login';
 
-describe('Login click', () => {
-  it('generates an onClick on Button press', () => {
+describe('Login correctly', () => {
+  it('logs in', () => {
     const loginSpy = jest.fn();
-    render(<Login />);
+    const mockedInitialize = jest.fn();
+    const mockedRequestCode = jest.fn();
+    const mockedInitCodeClient = jest.fn(() => ({
+      initialize: mockedInitialize,
+      requestCode: mockedRequestCode,
+    }));
 
+    window.google = {
+      accounts: {
+        id: { initialize: mockedInitialize },
+        oauth2: {
+          initCodeClient: mockedInitCodeClient,
+        },
+      },
+    };
+
+    render(<Login />);
     const button = screen.getByText('Login');
     button.onclick = loginSpy;
-    fireEvent.click(button);
 
+    fireEvent.click(button);
     expect(loginSpy).toBeCalled();
+
+    act(() => {
+      document.scripts[0].onload();
+    });
+
+    expect(mockedInitialize).toBeCalled();
+    expect(() =>
+      mockedInitialize.mock.calls[0][0].callback({ credential: {} })
+    ).toThrow();
+    expect(() =>
+      mockedInitCodeClient.mock.calls[0][0].callback({ error: 'error' })
+    ).toThrow();
+    expect(mockedInitCodeClient.mock.calls[0][0].redirect_uri).toBe(
+      'https://bemstudios.uk/auth'
+    );
+
+    expect(mockedRequestCode).toBeCalled();
+    expect(() => document.scripts[0].onerror()).toThrow();
+
+    fireEvent.click(button);
+    expect(mockedRequestCode).toBeCalledTimes(2);
+  });
+
+  it('goes to home after login', () => {
+    document.cookie = 'auth=123';
+    render(<Login />);
+    fireEvent.click(screen.getByText('Home'));
   });
 });
