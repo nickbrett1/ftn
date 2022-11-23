02ea0be236a1c6e7f93b75a2a4653871a516237d
Nick Brett <nick.brett1@gmail.com>
2022-11-23 03:33:31 +0000

    Additional worker tests

--- webapp/__tests__/index.test.js
+++ webapp/__tests__/index.test.js
@@ -1,25 +1,80 @@
+import { createServer } from 'miragejs';
 import index from '../worker/src/index';
 
 jest.mock('toucan-js');
 
+const env = {
+  SENTRY_ENVIRONMENT: 'development',
+  GOOGLE_CLIENT_SECRET: 123,
+  GOOGLE_CLIENT_ID: 123,
+  KV: { get: () => 'allowed', put: () => {} },
+};
+
+let server;
+beforeEach(() => {
+  server = createServer({
+    routes() {
+      this.get('https://bemstudios.uk', () => ({}));
+      this.post('https://oauth2.googleapis.com/token', () => ({ json: '' }));
+      this.get('https://www.googleapis.com/oauth2/v2/userinfo', () => ({
+        verified_email: true,
+        email: 'nick.brett1@gmail.com',
+      }));
+    },
+  });
+});
+afterEach(() => {
+  server.shutdown();
+});
+
 describe('Worker routing', () => {
   it('creates CSP', async () => {
     const mockedRedirect = jest.fn();
-    global.Response = jest
-      .fn()
-      .mockImplementation(() => ({ headers: new Headers() }));
     global.Response.redirect = mockedRedirect;
 
-    global.fetch = jest.fn(() => ({ body: {} }));
-
     const response = await index.fetch(
-      { url: 'https://bemstudios.uk' },
-      { SENTRY_ENVIRONMENT: 'development' },
-      {}
+      new Request('https://bemstudios.uk'),
+      env
     );
-
     expect(response.headers.get('Content-Security-Policy')).toEqual(
       expect.anything()
     );
+    expect(mockedRedirect).not.toBeCalled();
+  });
+
+  it('redirects to preview if not logged in', async () => {
+    const mockedRedirect = jest.fn(() => ({ body: '' }));
+    global.Response.redirect = mockedRedirect;
+
+    await index.fetch(new Request('https://bemstudios.uk/home'), env, {});
+    expect(mockedRedirect).toBeCalledWith('http://localhost:8787/preview', 307);
+  });
+
+  it('auth allows access', async () => {
+    const response = await index.fetch(
+      new Request('https://bemstudios.uk/auth?code=123'),
+      env,
+      {}
+    );
+    expect(response.headers.get('Location')).toEqual(
+      'http://localhost:8787/home'
+    );
+  });
+
+  it('auth denies access if not in KV', async () => {
+    const mockedRedirect = jest.fn(() => ({ body: '' }));
+    global.Response.redirect = mockedRedirect;
+
+    await index.fetch(
+      new Request('https://bemstudios.uk/auth?code=123'),
+      {
+        SENTRY_ENVIRONMENT: 'development',
+        GOOGLE_CLIENT_SECRET: 123,
+        GOOGLE_CLIENT_ID: 123,
+        KV: { get: () => 'notallowed', put: () => {} },
+      },
+      {}
+    );
+    expect(mockedRedirect).toBeCalledWith('http://localhost:8787/preview', 307);
   });
 });
