9253a0cdf956be7f61bd846642aaba00149a70aa
Nick Brett <nick.brett1@gmail.com>
2023-01-14 22:47:33 +0000

    With a basic story setup

--- webapp/.storybook/preview.js
+++ webapp/.storybook/preview.js
@@ -1,9 +1,27 @@
+import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../mui/theme';
+import { createClient, Provider } from 'urql';
+
+const client = createClient({ url: 'http://localhost:8787/graphql' });
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Provider value={client}>
+        <Story />
+      </Provider>
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
