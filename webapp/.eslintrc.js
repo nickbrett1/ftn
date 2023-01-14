8429340f06cccbfedb185fb21f45232e22f65fdc
Nick Brett <nick.brett1@gmail.com>
2023-01-14 20:18:41 +0000

    storybook setup

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -2,35 +2,45 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
-  extends: ['plugin:@next/next/recommended', 'airbnb', 'prettier'],
+  extends: ['plugin:@next/next/recommended', 'airbnb', 'prettier', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', 'react-hooks'],
   rules: {
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: true
+    }],
     'implicit-arrow-linebreak': 'off',
-    'react/no-unknown-property': ['error', { ignore: ['css'] }],
-    'object-curly-newline': [
-      'error',
-      {
-        ObjectExpression: { consistent: true, multiline: true },
-        ObjectPattern: { consistent: true, multiline: true },
-        ImportDeclaration: 'always-multiline',
-        ExportDeclaration: { multiline: true, minProperties: 3 },
+    'react/no-unknown-property': ['error', {
+      ignore: ['css']
+    }],
+    'object-curly-newline': ['error', {
+      ObjectExpression: {
+        consistent: true,
+        multiline: true
       },
-    ],
-    'react/jsx-wrap-multilines': [
-      'error',
-      { declaration: false, assignment: false },
-    ],
+      ObjectPattern: {
+        consistent: true,
+        multiline: true
+      },
+      ImportDeclaration: 'always-multiline',
+      ExportDeclaration: {
+        multiline: true,
+        minProperties: 3
+      }
+    }],
+    'react/jsx-wrap-multilines': ['error', {
+      declaration: false,
+      assignment: false
+    }],
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 };
