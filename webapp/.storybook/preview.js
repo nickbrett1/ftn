3ed30017ba7bc48136c669751dd3748cd57d9a6b
Nick Brett <nick.brett1@gmail.com>
2023-01-19 01:16:07 +0000

    Fix csp for fonts

--- webapp/.storybook/preview.js
+++ webapp/.storybook/preview.js
@@ -1,5 +1,9 @@
 import { ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import theme from '../mui/theme';
 import { createClient, Provider } from 'urql';
 import * as NextImage from 'next/image';
