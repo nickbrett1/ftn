c283178147f4c1856b2412deecf37f01a1db7d7f
Nick Brett <nick.brett1@gmail.com>
2023-01-21 00:42:50 +0000

    Upgrade sb for security

--- webapp/.storybook/main.js
+++ webapp/.storybook/main.js
@@ -1,16 +1,19 @@
+const path = require('path');
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  "framework": {
+    "name": "@storybook/nextjs",
+    "options": {}
   },
-  staticDirs: ['../public'],
-};
+  "docs": {
+    "autodocs": "tag"
+  }
+}
