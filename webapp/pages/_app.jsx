849bdfacd587608adce42763017301ed9128be5a
Nick Brett <nick.brett1@gmail.com>
2023-01-28 22:50:22 +0000

    Remove viewpoer from _app

--- webapp/pages/_app.jsx
+++ webapp/pages/_app.jsx
@@ -2,16 +2,8 @@
 /* eslint-disable react/prop-types */
 import 'normalize.css/normalize.css';
 import '../app/globals.css';
-import Head from 'next/head';
 import React from 'react';
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
