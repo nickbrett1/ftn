0bb879ef995a42049d26f56f76d369d355986dee
Nick Brett <nick.brett1@gmail.com>
2023-01-28 22:30:19 +0000

    Fixed for images on landing page

--- webapp/pages/index.jsx
+++ webapp/pages/index.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
 import Head from 'next/head';
-import styled from 'styled-components';
+import Image from 'next/image';
 import Login from '../components/Login';
-
-const Background = styled.div`
-  width: 100vw;
-  height: 100%;
-  background-image: url(/images/unionjack-extra-large.webp);
-  position: absolute;
-  background-repeat: no-repeat;
-  object-fit: cover;
-
-  @media screen and (max-width: 1536px) {
-    background-image: url(/images/unionjack-large.webp);
-  }
-  @media screen and (max-width: 1200px) {
-    background-image: url(/images/unionjack-medium.webp);
-  }
-  @media screen and (max-width: 900px) {
-    background-image: url(/images/unionjack-small.webp);
-  }
-  @media screen and (max-width: 600px) {
-    background-image: url(/images/unionjack-extra-small.webp);
-  }
-`;
+import background from '../public/images/unionjack-extra-large.webp';
 
 export default function LandingFrame() {
   return (
-    <>
+    <div className="block">
       <Head>
         <meta
           name="description"
@@ -35,16 +14,15 @@ export default function LandingFrame() {
         />
         <title>British Empire Management</title>
       </Head>
-      <Background>
-        <nav className="flex items-center bg-stone-800 p-4">
-          <p className="whitespace-nowrap text-2xl tracking-tight text-white">
-            British Empire Management
-          </p>
-          <div className="mr-0 ml-auto">
-            <Login />
-          </div>
-        </nav>
-      </Background>
-    </>
+      <nav className="flex items-center bg-stone-800 p-4">
+        <p className="whitespace-nowrap text-2xl tracking-tight text-white">
+          British Empire Management
+        </p>
+        <div className="mr-0 ml-auto">
+          <Login />
+        </div>
+      </nav>
+      <Image src={background} layout="responsive" priority />
+    </div>
   );
 }
