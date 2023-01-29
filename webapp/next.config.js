ed0718dd454b55a5e83d98bd5f2ac4eb51c6e4d7
Nick Brett <nick.brett1@gmail.com>
2023-01-29 18:46:32 +0000

    Try disabling pwa on prod

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -6,7 +6,9 @@
 const { withSentryConfig } = require('@sentry/nextjs');
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  disable:
+    process.env.NODE_ENV === 'development' ||
+    process.env.NODE_ENV === 'production',
 });
 
 const moduleExports = withPWA({
