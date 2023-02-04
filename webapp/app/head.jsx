a93d5bd1ce8d6860588381c1ef42b1fa34b53248
Nick Brett <nick.brett1@gmail.com>
2023-02-04 18:07:24 +0000

    Add meta name viewport

--- webapp/app/head.jsx
+++ webapp/app/head.jsx
@@ -10,6 +10,7 @@ export default function Head() {
       <link rel="manifest" href="/manifest.webmanifest" />
 
       <meta name="theme-color" content="black" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/icons/favicon.ico" sizes="any" />
       <link rel="icon" type="image/svg+xml" href="/icons/square-flag.svg" />
       <link rel="apple-touch-icon" href="./icons/apple-touch-icon.png" />
