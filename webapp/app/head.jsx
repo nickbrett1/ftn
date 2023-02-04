9e20162de94362bf6c7ba5ed22ef57014083997a
Nick Brett <nick.brett1@gmail.com>
2023-02-04 01:35:55 +0000

    Fix service worker registration

--- webapp/app/head.jsx
+++ webapp/app/head.jsx
@@ -7,6 +7,17 @@ export default function Head() {
         name="description"
         content="Home automation and collaborative family household management"
       />
+
+      <meta name="theme-color" content="black" />
+      <link rel="manifest" href="/manifest.webmanifest" />
+      <link rel="icon" href="/icons/favicon.ico" sizes="any" />
+      <link rel="icon" type="image/svg+xml" href="/icons/square-flag.svg" />
+      <link rel="apple-touch-icon" href="./icons/apple-touch-icon.png" />
+      <link
+        rel="preconnect"
+        href="https://res.cloudinary.com"
+        crossOrigin="true"
+      />
       <title>British Empire Management</title>
     </>
   );
--- webapp/app/layout.jsx
+++ webapp/app/layout.jsx
@@ -10,16 +10,6 @@ export default function RootLayout({ children }) {
         <noscript>
           <meta httpEquiv="refresh" content="0; url=./nojs/page" />
         </noscript>
-        <meta name="theme-color" content="black" />
-        <link rel="manifest" href="/manifest.webmanifest" />
-        <link rel="icon" href="/icons/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/svg+xml" href="/icons/square-flag.svg" />
-        <link rel="apple-touch-icon" href="./icons/apple-touch-icon.png" />
-        <link
-          rel="preconnect"
-          href="https://res.cloudinary.com"
-          crossOrigin="true"
-        />
       </head>
       <body>{children}</body>
     </html>
