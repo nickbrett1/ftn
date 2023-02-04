9d2ede44fd9396123c92488e6fef6316acc60176
Nick Brett <nick.brett1@gmail.com>
2023-02-04 01:29:34 +0000

    Fix icons

--- webapp/app/layout.jsx
+++ webapp/app/layout.jsx
@@ -6,6 +6,21 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <noscript>
+          <meta httpEquiv="refresh" content="0; url=./nojs/page" />
+        </noscript>
+        <meta name="theme-color" content="black" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="icon" href="/icons/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/svg+xml" href="/icons/square-flag.svg" />
+        <link rel="apple-touch-icon" href="./icons/apple-touch-icon.png" />
+        <link
+          rel="preconnect"
+          href="https://res.cloudinary.com"
+          crossOrigin="true"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
