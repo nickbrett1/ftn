fd85d92685e2de538e533f5406cd58f58aa7b5a9
Nick Brett <nick.brett1@gmail.com>
2023-03-28 01:45:41 +0000

    Now with bubbles!

--- webapp/src/lib/components/Background.svelte
+++ webapp/src/lib/components/Background.svelte
@@ -1,7 +1,41 @@
 <script>
 	import fintechgreen from '$lib/assets/images/fintechgreen.png?run';
 	import Img from '@zerodevx/svelte-img';
+	import { onMount } from "svelte";
+	import { loadBigCirclesPreset } from "tsparticles-preset-big-circles";
+
+	let particlesConfig = {
+    preset: "bigCircles", 
+		fullScreen: {
+			enable: true,
+			zIndex: -1,
+		},
+	};
+
+	let ParticlesComponent;
+
+	onMount(async () => {
+		const module = await import("svelte-particles");
+		ParticlesComponent = module.default;
+	});
+
+	let onParticlesLoaded = event => {
+	};
+
+	let particlesInit = async main => {
+		await loadBigCirclesPreset(main);
+	};
+
 </script>
 
 <Img src={fintechgreen} loading="eager" alt="background" /> 
-<div class="bg-green-300 w-screen h-screen animate-fromto"></div>
+
+<svelte:component
+	this="{ParticlesComponent}"
+	id="tsparticles"
+	options="{particlesConfig}"
+	on:particlesLoaded="{onParticlesLoaded}"
+	particlesInit="{particlesInit}"
+/>
+
+
