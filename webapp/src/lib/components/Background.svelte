38719e879872d295bfb64331201ebf1f7bcd9570
Nick Brett <nick.brett1@gmail.com>
2023-04-06 02:24:50 +0000

    Now with percentage changes

--- webapp/src/lib/components/Background.svelte
+++ webapp/src/lib/components/Background.svelte
@@ -1,6 +1,7 @@
 <script>
 	import fintechgreen from '$lib/assets/images/fintechgreen.png?run';
 	import Img from '@zerodevx/svelte-img';
+  import { random } from 'nanoid';
 	import { onMount } from "svelte";
 	import { loadFull } from 'tsparticles';
 
@@ -14,24 +15,24 @@
   interactivity: {
     detect_on: "canvas",
     events: {
-      onclick: { enable: true, mode: "push" },
+      onclick: { enable: false, mode: "push" },
       onhover: {
         enable: true,
-        mode: "repulse",
+        mode: "connect",
         parallax: { enable: false, force: 60, smooth: 10 }
       },
       resize: true
     },
     modes: {
       bubble: { distance: 400, duration: 2, opacity: 0.8, size: 40, speed: 3 },
-      grab: { distance: 400, links: { opacity: 1 } },
+      grab: { distance: 400, lineLinked: { opacity: 1 } },
       push: { quantity: 4 },
       remove: { quantity: 2 },
-      repulse: { distance: 200, duration: 0.4 }
+      repulse: { distance: 200, duration: 0.4 },
+			connect: { distance: 400, links: { opacity: 0.5 }, radius: 150 },
     }
   },
   particles: {
-    color: { value: "random" },
     links: {
       color: "random",
       distance: 150,
@@ -41,49 +42,62 @@
     },
     move: {
       attract: { enable: false, rotateX: 600, rotateY: 1200 },
-      bounce: false,
-      direction: "none",
+      bounce: true,
+      direction: "top",
       enable: true,
       out_mode: "out",
       random: false,
-      speed: 3,
+      speed: 1,
       straight: false
     },
     rotate: {
       animation: {
-        enable: true,
+        enable: false,
         speed: 10,
         sync: false
       }
     },
     number: { density: { enable: true, area: 800 }, value: 20 },
     opacity: {
-      animation: { enable: true, minimumValue: 0.5, speed: 1, sync: false },
+      animation: { enable: true, speed: 0.5, sync: false },
       random: false,
-      value: 1
+      value: { min: 0.1, max: 0.8 }
     },
     shape: {
-      character: [
-        {
-          fill: true,
-          font: "Verdana",
-          style: "",
-          value: ["hello", "world"],
-          weight: "400"
-        },
-      ],
-      polygon: { nb_sides: 5 },
-      stroke: { color: "random", width: 1 },
+			options: {
+				character: [
+					{
+						fill: true,
+						font: "Verdana",
+						// Generate array of 50 random numbers between 0.1 and 15 rounded to two decimal places
+						value: Array.from({ length: 50 }, () => ("+"+Math.round(Math.random() * 15 * 100) / 100 )+"%"),
+						weight: "400",
+						particles: {
+							color: "#00FF9E",
+							size: {
+      					value: 8
+							}
+						}
+					},
+					{
+						fill: true,
+						font: "Verdana",
+						value: Array.from({ length: 50 }, () => ("-"+Math.round(Math.random() * 15 * 100) / 100 )+"%"),
+						weight: "400",
+						particles: {
+							color: "#FF0061",
+							size: {
+      					value: 8
+							}
+						}
+					},
+      	],
+			},
       type: "char"
     },
-    size: {
-      anim: { enable: true, minimumValue: 8, speed: 20, sync: false },
-      random: { minimumValue: 8, enable: true },
-      value: 18
-    }
   },
   detectRetina: true
-};
+	};
 
 
 	let ParticlesComponent;
@@ -94,7 +108,6 @@
 	});
 
 	let onParticlesLoaded = event => {
-		console.log("Particles loaded", event.detail);
 	};
 
 	let particlesInit = async main => {
@@ -102,8 +115,6 @@
 	};
 
 </script>
-
-<Img src={fintechgreen} loading="eager" alt="background" /> 
 
 <svelte:component
 	this="{ParticlesComponent}"
