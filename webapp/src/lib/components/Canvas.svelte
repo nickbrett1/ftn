f9d2c40eeb09d647b7528eee399bbad22f5c2fc0
Nick Brett <nick.brett1@gmail.com>
2023-04-09 02:20:27 +0000

    Try removing degToRad

--- webapp/src/lib/components/Canvas.svelte
+++ webapp/src/lib/components/Canvas.svelte
@@ -2,7 +2,6 @@
 
 	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core'
 	import { spring } from 'svelte/motion'
-	import { degToRad } from 'three/src/math/MathUtils'
 
 	const scale = spring(1)
 </script>
@@ -10,7 +9,7 @@
 <div class="w-full h-full">
 	<Canvas>
 		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
+			<OrbitControls maxPolarAngle={1.39626} enableZoom={false} target={{ y: 0.5 }} />
 		</T.PerspectiveCamera>
 
 		<T.DirectionalLight castShadow position={[3, 10, 10]} />
@@ -34,7 +33,7 @@
 		</T.Group>
 
 		<!-- Floor -->
-		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
+		<T.Mesh receiveShadow rotation.x={-1.5708}>
 			<T.CircleGeometry args={[3, 72]} />
 			<T.MeshStandardMaterial color="white" />
 		</T.Mesh>
