f6bf6ef12d6caad08ed82d7dda8ed472e8b3cc4b
Nick Brett <nick.brett1@gmail.com>
2023-04-11 02:02:21 +0000

    Fix lighthouse

--- webapp/.lighthouserc.cjs
+++ webapp/.lighthouserc.cjs
@@ -7,15 +7,7 @@ module.exports = {
 			extends: 'lighthouse:default',
 			settings: {
 				cpuSlowdownMultiplier: 2.4,
-				chromeFlags: '--no-sandbox',
-				formFactor: 'desktop',
-				screenEmulation: {
-					mobile: false, // Mobile doesn't detect binary files for compression: https://github.com/GoogleChrome/lighthouse/issues/9826
-					width: 1350,
-					height: 940,
-					deviceScaleFactor: 1,
-					disabled: false
-				}
+				chromeFlags: '--no-sandbox'
 			}
 		},
 		upload: {
@@ -24,7 +16,8 @@ module.exports = {
 		assert: {
 			preset: 'lighthouse:recommended',
 			assertions: {
-				'unused-javascript': 'off' // Three.JS pulls in a lot of unused JS
+				'unused-javascript': 'off', // Three.JS pulls in a lot of unused JS
+				'uses-text-compression': 'off' // Doesn't detect binary files when considering compression: https://github.com/GoogleChrome/lighthouse/issues/9826
 			}
 		}
 	}
