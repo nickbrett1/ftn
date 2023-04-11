993ca5196af98a03b15eaa2e518126a831dfc869
Nick Brett <nick.brett1@gmail.com>
2023-04-11 01:39:31 +0000

    Force to desktop lighthouse

--- webapp/.lighthouserc.cjs
+++ webapp/.lighthouserc.cjs
@@ -6,7 +6,14 @@ module.exports = {
 			url: ['http://localhost:4173'],
 			settings: {
 				cpuSlowdownMultiplier: 2.4,
-				chromeFlags: '--no-sandbox'
+				chromeFlags: '--no-sandbox',
+				screenEmulation: {
+					mobile: false, // Mobile doesn't detect binary files for compression: https://github.com/GoogleChrome/lighthouse/issues/9826
+					width: 1350,
+					height: 940,
+					deviceScaleFactor: 1,
+					disabled: false
+				}
 			}
 		},
 		upload: {
@@ -15,7 +22,8 @@ module.exports = {
 		assert: {
 			preset: 'lighthouse:recommended',
 			assertions: {
-				'unused-javascript': 'off' // Three.JS pulls in a lot of unused JS
+				'unused-javascript': 'off', // Three.JS pulls in a lot of unused JS
+				'uses-text-compression': 'off' // Cloudflare handles this
 			}
 		}
 	}
