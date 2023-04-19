85bae5d1a49f9418b7adfa0c5d3548875aaba1f6
Nick Brett <nick.brett1@gmail.com>
2023-04-19 01:30:46 +0000

    Added useFrame support

--- webapp/src/lib/components/Canvas.svelte
+++ webapp/src/lib/components/Canvas.svelte
@@ -1,36 +1,12 @@
 <script>
+	import Scene from '$lib/components/Scene.svelte'
 	import { 
 		Canvas, 
-		OrbitControls, 
-		PerspectiveCamera, 
-		AmbientLight, 
-		DirectionalLight
 	} from '@threlte/core'
-	import { GLTF } from '@threlte/extras'
-	import model from '$lib/assets/models/monitors.glb'
-
 </script>
 
 <div class="w-full h-full">
 	<Canvas>
-
-		<PerspectiveCamera position={{ x: -1400, y: 0, z: 0 }} lookAt= {{x:0, y:0, z:0 }}  fov={45}>
-			<OrbitControls enableDamping/>
-		</PerspectiveCamera>
-
-		<DirectionalLight position={{ x: -100, y: 500, z: 0 }} 
-			color={0x39FF14}
-			intensity={0.7} />
-		<AmbientLight intensity={0.2} />
-		<GLTF 
-			position={{x: 0, y: 0, z:60 }} 
-			url={model} 
-			useDraco={true} 
-			interactive 
-			on:click={() => {
-				console.log('user clicked')
-			}} 
-		/>
-
+		<Scene />
 	</Canvas>
 </div>
---
+++ webapp/src/lib/components/Scene.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { 
+		OrbitControls, 
+		PerspectiveCamera, 
+		AmbientLight, 
+		DirectionalLight,
+		useFrame
+	} from '@threlte/core'
+	import { GLTF } from '@threlte/extras'
+	import model from '$lib/assets/models/monitors.glb'
+
+	let rotation = 0
+
+	useFrame(() => {
+	})
+
+</script>
+
+<PerspectiveCamera position={{ x: -1400, y: 0, z: 0 }} lookAt= {{x:0, y:0, z:0 }}  fov={45}>
+	<OrbitControls enableDamping/>
+</PerspectiveCamera>
+
+<DirectionalLight position={{ x: -1000, y: 100, z: 200 }} target={{ x: 0, y: 0, z: 0 }} 
+	color={0x39FF14}
+	intensity={0.6}
+/>
+<AmbientLight intensity={0.2} />
+<GLTF 
+	position={{x: 0, y: 0, z:60 }} 
+	url={model} 
+	useDraco={true} 
+	interactive 
+	on:click={() => {
+		console.log('user clicked')
+	}} 
+/>
