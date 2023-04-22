f4699b1120d32f327329213507bfcf5acf71fdbd
Nick Brett <nick.brett1@gmail.com>
2023-04-22 19:43:07 +0000

    Ignore unused javascript

--- webapp/.lighthouserc.cjs
+++ webapp/.lighthouserc.cjs
@@ -16,6 +16,7 @@ module.exports = {
 		assert: {
 			preset: 'lighthouse:recommended',
 			assertions: {
+				'unused-javascript': 'off', // Three.JS pulls in a lot of unused JS
 				'uses-text-compression': 'off' // Doesn't detect binary files when considering compression: https://github.com/GoogleChrome/lighthouse/issues/9826
 			}
 		}
