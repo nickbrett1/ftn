57fa5cd13515feb251dd058c036522b9cd7f8fa9
Nick Brett <nick.brett1@gmail.com>
2023-04-23 01:38:20 +0000

    Office model

--- webapp/src/lib/components/Scene.svelte
+++ webapp/src/lib/components/Scene.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { T, useFrame } from '@threlte/core'
-	import { Float, OrbitControls, useGltf, interactivity } from '@threlte/extras'
-	import model from '$lib/assets/models/monitors.glb'
+	import { Float, GLTF, OrbitControls, useGltf, interactivity } from '@threlte/extras'
+	import model from '$lib/assets/models/office.glb'
 
 	interactivity()
 
@@ -16,49 +16,28 @@
 </script>
 
 <T.PerspectiveCamera 
-	position={[-1400, 0, 0]} 
-	rotation={[0, 0, 0]} 
+	position={[6, 8, 9]} 
 	fov={50} 
 	aspect={window.innerWidth / window.innerHeight} 
 	near={0.1} 
 	far={10000}
 	makeDefault>
-	<OrbitControls enableDamping enableZoom={false}/>
+	<OrbitControls enableDamping autoRotate autoRotateSpeed={0.2} />
 </T.PerspectiveCamera>
 
 <T.DirectionalLight 
-	position={[-1000, 100, 200 ]} 
-	color="#50C878"
+	position={[6, 8, 9]} 
+	color="white"
 	intensity={0.6}
 />
 
 <T.AmbientLight intensity={0.2} />
 
-{#if $gltf}
-<Float f
-	floatIntensity={5}   
-	rotationIntensity={1}
-  rotationSpeed={[0.5, 0.5, 0.5]}>
-	<T is={$gltf.nodes['Left'] }  
-		position={[-95, 115, -10] }   
-		on:click={(e) => {
-			e.stopPropagation()
-    console.log('left clicked')
-  }}/>
-	<T is={$gltf.nodes['Middle'] } 
-		position={[-95, 115, -10] } 
-		on:click={(e) => {
-			e.stopPropagation()
-    console.log('middle clicked')
-  }} />
-	<T is={$gltf.nodes['Right'] } 
-		position={[-95, 115, -10] } 
-		on:click={(e) => {
-			e.stopPropagation()
-    console.log('right clicked')
-  }} />
-	<T is={$gltf.nodes['Stand'] } position={[-95, 115, -10] } />
-</Float>
-{/if}
+<GLTF url={model}
+	on:load={({materials, nodes}) => { 
+		console.log('nodes', nodes)
+		console.log('materials', materials)
+	}} />
+
 
 
