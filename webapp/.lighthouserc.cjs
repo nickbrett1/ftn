d7fb1c72fc402715d855e101fb35032897699561
Nick Brett <nick.brett1@gmail.com>
2023-05-04 01:45:02 +0000

    Fix valid-source-maps check

--- webapp/.lighthouserc.cjs
+++ webapp/.lighthouserc.cjs
@@ -17,7 +17,8 @@ module.exports = {
 			preset: 'lighthouse:recommended',
 			assertions: {
 				'unused-javascript': 'off', // Three.JS pulls in a lot of unused JS
-				'uses-text-compression': 'off' // Doesn't detect binary files when considering compression: https://github.com/GoogleChrome/lighthouse/issues/9826
+				'uses-text-compression': 'off', // Doesn't detect binary files when considering compression: https://github.com/GoogleChrome/lighthouse/issues/9826
+				'valid-source-maps': 'off' // We have sourcemaps but loading can be too slow to detect
 			}
 		}
 	}
