614e62551dccb7d8e0a73d331750d44227a459b5
Nick Brett <nick.brett1@gmail.com>
2023-05-05 21:54:22 +0000

    Try adding sourcemap to workbox

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -12,6 +12,10 @@ export default defineConfig({
 			defaultDirectives: () => new URLSearchParams(`?width=480;960;1024;1920&format=avif;webp;jpg`)
 		}),
 		SvelteKitPWA({
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,svg,png,jpg,jpeg,webp,avif}'],
+				sourcemap: true
+			},
 			manifest: {
 				name: 'Fintech Nick',
 				icons: [
