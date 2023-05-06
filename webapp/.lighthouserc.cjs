1377c49873907d59231bd29a6cc0c637fa8ed21a
Nick Brett <nick.brett1@gmail.com>
2023-05-06 00:27:57 +0000

    Try again with sourcemaps now fixed pwa

--- webapp/.lighthouserc.cjs
+++ webapp/.lighthouserc.cjs
@@ -17,8 +17,7 @@ module.exports = {
 			preset: 'lighthouse:recommended',
 			assertions: {
 				'unused-javascript': 'off', // Three.JS pulls in a lot of unused JS
-				'uses-text-compression': 'off', // Doesn't detect binary files when considering compression: https://github.com/GoogleChrome/lighthouse/issues/9826
-				'valid-source-maps': 'off' // We have sourcemaps but loading can be too slow to detect
+				'uses-text-compression': 'off' // Doesn't detect binary files when considering compression: https://github.com/GoogleChrome/lighthouse/issues/9826
 			}
 		}
 	}
