9bb30cbc2b152d169446d87dc53c0646c1155b60
Nick Brett <nick.brett1@gmail.com>
2023-08-05 14:09:42 +0000

    Upgrade @threlte/core

--- webapp/src/routes/index.test.js
+++ webapp/src/routes/index.test.js
@@ -1,4 +1,6 @@
-import { expect, describe, it } from 'vitest';
+import { expect, describe, it, vi } from 'vitest';
+import ResizeObserver from 'resize-observer-polyfill';
+
 import { render } from '@testing-library/svelte';
 import { screen } from '@testing-library/dom';
 import App from './+page.svelte';
@@ -7,6 +9,8 @@ import App from './+page.svelte';
  * @vitest-environment jsdom
  */
 describe('App', () => {
+	vi.stubGlobal('ResizeObserver', ResizeObserver);
+
 	it('renders the app', () => {
 		render(App);
 
