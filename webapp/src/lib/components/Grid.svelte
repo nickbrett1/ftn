6ce4a290a83c21b644ad2665ac43f94972ae5be3
Nick Brett <nick.brett1@gmail.com>
2024-07-16 01:40:33 +0000

    Fix contrast

--- webapp/src/lib/components/Grid.svelte
+++ webapp/src/lib/components/Grid.svelte
@@ -33,11 +33,13 @@
 	<Portfolio
 		title="Our work"
 		subtitle="Crafted with skill and care to help our clients grow their business!"
+		subtitleClass="mt-4 text-base font-normal text-white sm:text-xl dark:text-gray-400'"
 		h2Class="text-3xl font-extrabold leading-tight tracking-tight text-white sm:text-4xl dark:text-white"
 	>
 		{#each portfolios as item}
 			<PortfolioItem
 				titleClass="text-2xl font-bold leading-tight text-white dark:text-white"
+				descriptionClass="text-lg font-normal text-white dark:text-gray-400"
 				{item}
 			/>
 		{/each}
