a7c031fdba193518f5c68d159dacae0ca9013fb9
Nick Brett <nick.brett1@gmail.com>
2024-07-19 15:05:31 +0000

    Move 3d to own page

--- webapp/src/routes/projects/3d/+page.svelte
+++ webapp/src/routes/projects/3d/+page.svelte
@@ -1,9 +1,9 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
-	import Background from '$lib/components/Background.svelte';
+	import Canvas from '$lib/components/Canvas.svelte';
 </script>
 
 <div>
 	<Header />
-	<Background />
+	<Canvas />
 </div>
