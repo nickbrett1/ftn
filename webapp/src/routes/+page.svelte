38abf891b753a6d7ff6d52577fa5778bf41004a6
Nick Brett <nick.brett1@gmail.com>
2024-07-22 01:19:29 +0000

    Landing page improvements

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import Background from '$lib/components/Background.svelte';
+	import Landing from '$lib/components/Landing.svelte';
 </script>
 
 <div>
 	<Header />
 	<Background />
+	<Landing />
 </div>
