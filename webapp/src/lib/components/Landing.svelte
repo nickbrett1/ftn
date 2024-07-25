a4e8916cf557d04fb076c5329eae1ea04b7eec28
Nick Brett <nick.brett1@gmail.com>
2024-07-25 00:55:37 +0000

    Bit bigger text

--- webapp/src/lib/components/Landing.svelte
+++ webapp/src/lib/components/Landing.svelte
@@ -15,7 +15,7 @@
 	<div class="text-white">
 		{#each lines as line, i}
 			{#if animate}
-				<div class="max-w-4xl sm:text-5xl text-4xl font-black text-center">
+				<div class="max-w-4xl sm:text-6xl text-4xl font-black text-center">
 					<div class="inline-block overflow-hidden align-bottom">
 						<span
 							class="inline-block m-1 uppercase drop-shadow-xl"
