5e8f7dbc232a3848d3f5abe87bc7183d5ac8fe00
Nick Brett <nick.brett1@gmail.com>
2024-09-09 02:28:35 +0000

    Scrolling and timeline begins

---
+++ webapp/src/lib/components/Timeline.svelte
@@ -0,0 +1,32 @@
+<script>
+	import {
+		Timeline,
+		TimelineItem,
+		TimelineSeparator,
+		TimelineDot,
+		TimelineConnector,
+		TimelineContent
+	} from 'svelte-vertical-timeline';
+
+	const options = [{ title: 'Eat' }, { title: 'Sleep' }, { title: 'Code' }];
+</script>
+
+<Timeline>
+	<!-- 
+		{#each options as option}
+		<TimelineItem>
+			<TimelineSeparator>
+				<TimelineDot style="background-color: white" />
+				<TimelineConnector />
+			</TimelineSeparator>
+			<TimelineContent>
+				<h3
+					class="text-white sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-10xl font-white"
+				>
+					{option.title}
+				</h3>
+			</TimelineContent>
+		</TimelineItem>
+	{/each}
+-->
+</Timeline>
--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -2,10 +2,23 @@
 	import Header from '$lib/components/Header.svelte';
 	import Background from '$lib/components/Background.svelte';
 	import Landing from '$lib/components/Landing.svelte';
+	import Timeline from '$lib/components/Timeline.svelte';
 </script>
+
+<Background />
 
 <div class="flex flex-col h-dvh">
 	<Header />
-	<Background />
 	<Landing />
+</div>
+
+<div class="h-dvh flex flex-col">
+	<div
+		class="text-white z-10 py-10 text-center sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-10xl font-bold"
+	>
+		Let Me Introduce Myself
+	</div>
+	<div class="flex grow items-center">
+		<Timeline />
+	</div>
 </div>
