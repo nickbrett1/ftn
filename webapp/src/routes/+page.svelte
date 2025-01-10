9e36d0d2485417a822ab6e61d025eb3e670ccb37
Nick Brett <nick.brett1@gmail.com>
2025-01-10 01:47:20 +0000

    Better intro

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import Header from '$lib/components/Header.svelte';
 	import Background from '$lib/components/Background.svelte';
 	import Landing from '$lib/components/Landing.svelte';
-	import Timeline from '$lib/components/Timeline.svelte';
 
 	import { inview } from 'svelte-inview';
 	import { fade } from 'svelte/transition';
@@ -27,7 +26,7 @@
 	<Landing />
 </div>
 
-<div class="flex flex-col justify-center items-center grow">
+<div class="flex flex-col justify-center items-center grow h-dvh">
 	<div use:inview={options} on:inview_change={handleChange}>
 		<div
 			class:animate={isInView}
@@ -41,16 +40,8 @@
 				>
 					Let Me Introduce Myself
 				</div>
-			{:else}
-				<div
-					class="text-white text-opacity-0 z-10 py-5 max-w-4xl text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-10xl font-black text-center"
-				>
-					Let Me Introduce Myself
-				</div>
 			{/if}
 		</div>
 	</div>
-	<div class="flex grow items-center">
-		<!-- <Timeline /> -->
-	</div>
+	<div class="flex grow"></div>
 </div>
