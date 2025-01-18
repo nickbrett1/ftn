ca0a091fabca89bc93460db03855d90ae3da7fa9
Nick Brett <nick.brett1@gmail.com>
2025-01-18 02:03:20 +0000

    Cards with fancy light hover

---
+++ webapp/src/lib/components/Card.svelte
@@ -0,0 +1,107 @@
+<!-- Border wraps the inner card element -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+
+<!-- Thanks to Yash Verma, and his blog on animating a card hover for this: 
+ https://yashverma.me/blog/cards -->
+
+<script>
+	let rect = null;
+
+	function createClientRectTracker() {
+		const action = (node) => {
+			function update() {
+				rect = node.getBoundingClientRect();
+			}
+
+			const handle = setInterval(update, 100);
+			update();
+
+			return {
+				destroy() {
+					clearInterval(handle);
+				}
+			};
+		};
+
+		return action;
+	}
+
+	const clientRectTracker = createClientRectTracker();
+
+	let blob;
+
+	function handleMouseMove(ev) {
+		blob.style.opacity = '1';
+
+		blob.animate(
+			[
+				{
+					transform: `translate(${ev.clientX - rect.left - rect.width / 2}px,${ev.clientY - rect.top - rect.height / 2}px)`
+				}
+			],
+			{
+				duration: 300,
+				fill: 'forwards'
+			}
+		);
+	}
+</script>
+
+<svelte:window on:mousemove={handleMouseMove} />
+
+<button
+	on:click
+	class={`
+		bg-green-800/20
+		p-1
+		rounded-lg
+		relative
+		overflow-hidden
+	`}
+>
+	<div
+		class={`
+			cursor-pointer
+			rounded-lg
+			p-5
+			w-52
+			h-40
+			bg-gray-900
+			hover:bg-gray-900/60
+			overflow-hidden 
+			relative
+			transition-all`}
+	>
+		<slot />
+	</div>
+	<div
+		class={`
+			blur-2xl
+			absolute
+			-z-10
+			top-0
+			left-0
+			w-64
+			h-64
+			rounded-full
+			opacity-0
+			bg-white/50
+			transition-all
+		`}
+		bind:this={blob}
+	></div>
+
+	<div
+		class={`
+			invisible
+			absolute
+			-z-10
+			top-0
+			left-0
+			w-64
+			h-64
+			rounded-full
+		`}
+		use:clientRectTracker
+	></div>
+</button>
--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Header from '$lib/components/Header.svelte';
 	import Background from '$lib/components/Background.svelte';
 	import Landing from '$lib/components/Landing.svelte';
+	import Card from '$lib/components/Card.svelte';
 
 	import { inview } from 'svelte-inview';
 	import { fade } from 'svelte/transition';
@@ -21,12 +22,12 @@
 
 <Background />
 
-<div class="flex flex-col h-dvh">
+<div class="flex flex-col grow h-dvh min-h-max overflow-visible">
 	<Header />
 	<Landing />
 </div>
 
-<div class="flex flex-col justify-center items-center grow h-dvh">
+<div class="flex flex-col justify-center items-center grow">
 	<div use:inview={options} on:inview_change={handleChange}>
 		<div
 			class:animate={isInView}
@@ -43,5 +44,35 @@
 			{/if}
 		</div>
 	</div>
-	<div class="flex grow"></div>
+</div>
+
+<div
+	class={`w-full 
+		min-h-dvh 
+		m-0 
+		gap-2 
+		text-white 
+		flex 
+		flex-row 
+		flex-wrap 
+		justify-center 
+		content-center 
+		items-center`}
+>
+	<Card on:click={() => (window.location.href = '/projects')}>
+		<h1 class="text-xl mb-4 leading-none">Making of FinTechNick</h1>
+		<p class="text-left">How this site was made</p>
+	</Card>
+	<Card on:click={() => (window.location.href = '/projects')}>
+		<h1 class="text-xl mb-4 leading-none">Machine Learnings on GCP</h1>
+		<p class="text-left">A project about machine learning on GCP</p>
+	</Card>
+	<Card on:click={() => (window.location.href = '/techstack')}>
+		<h1 class="text-xl mb-4 leading-none">Tools & Technologies</h1>
+		<p class="text-left">Links to useful tools and technologies</p>
+	</Card>
+	<Card on:click={() => (window.location.href = '/blog')}>
+		<h1 class="text-xl mb-4 leading-none">Blog</h1>
+		<p class="text-left">Blog posts on software engineering topics</p>
+	</Card>
 </div>
