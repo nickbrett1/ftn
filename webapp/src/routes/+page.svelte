7dd12e40adcbff25a8f0341871dba9919acccca0
Nick Brett <nick.brett1@gmail.com>
2025-01-19 01:27:42 +0000

    Use svelte-awesome-icons and improve layout

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import Background from '$lib/components/Background.svelte';
 	import Landing from '$lib/components/Landing.svelte';
 	import Card from '$lib/components/Card.svelte';
+	import LinkedinInBrand from 'svelte-awesome-icons/LinkedinInBrand.svelte';
 
 	import { inview } from 'svelte-inview';
 	import { fade } from 'svelte/transition';
@@ -37,7 +38,7 @@
 			{#if isInView}
 				<div
 					in:fade={{ duration: 2000 }}
-					class="text-white z-10 py-5 max-w-4xl text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-10xl font-black text-center"
+					class="text-white z-10 px-8 py-5 max-w-4xl text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-10xl font-black text-center"
 				>
 					Let Me Introduce Myself
 				</div>
@@ -49,30 +50,68 @@
 <div
 	class={`w-full 
 		min-h-dvh 
-		m-0 
+		justify-center
+		content-center
+		items-center
+		flex
+		flex-col 
+		gap-14`}
+>
+	<div
+		class={`m-0 
 		gap-2 
 		text-white 
 		flex 
 		flex-row 
 		flex-wrap 
-		justify-center 
-		content-center 
-		items-center`}
->
-	<Card on:click={() => (window.location.href = '/projects')}>
-		<h1 class="text-xl mb-4 leading-none">Making of FinTechNick</h1>
-		<p class="text-left">How this site was made</p>
-	</Card>
-	<Card on:click={() => (window.location.href = '/projects')}>
-		<h1 class="text-xl mb-4 leading-none">Machine Learnings on GCP</h1>
-		<p class="text-left">A project about machine learning on GCP</p>
-	</Card>
-	<Card on:click={() => (window.location.href = '/techstack')}>
-		<h1 class="text-xl mb-4 leading-none">Tools & Technologies</h1>
-		<p class="text-left">Links to useful tools and technologies</p>
-	</Card>
-	<Card on:click={() => (window.location.href = '/blog')}>
-		<h1 class="text-xl mb-4 leading-none">Blog</h1>
-		<p class="text-left">Blog posts on software engineering topics</p>
-	</Card>
+		justify-center
+		`}
+	>
+		<Card on:click={() => (window.location.href = '/projects')}>
+			<h1 class="text-xl mb-4 leading-none">Making of FinTechNick</h1>
+			<p class="text-left">How this site was made</p>
+		</Card>
+		<Card on:click={() => (window.location.href = '/projects')}>
+			<h1 class="text-xl mb-4 leading-none">Machine Learnings on GCP</h1>
+			<p class="text-left">A project about machine learning on GCP</p>
+		</Card>
+		<Card on:click={() => (window.location.href = '/techstack')}>
+			<h1 class="text-xl mb-4 leading-none">Tools & Technologies</h1>
+			<p class="text-left">Links to useful tools and technologies</p>
+		</Card>
+		<Card on:click={() => (window.location.href = '/blog')}>
+			<h1 class="text-xl mb-4 leading-none">Blog</h1>
+			<p class="text-left">Blog posts on software engineering topics</p>
+		</Card>
+	</div>
+	<div
+		class={`m-0 
+			gap-2 
+			text-white 
+			flex 
+			flex-row 
+			flex-wrap 
+			justify-center`}
+	>
+		<Card on:click={() => (window.location.href = '/github')}>
+			<h1 class="text-xl mb-4 leading-none">Github</h1>
+			<p class="text-left">Github</p>
+		</Card>
+
+		<Card on:click={() => (window.location.href = '/linkedin')}>
+			<div class="flex justify-center content-center items-center gap-3 flex-col h-full w-full">
+				<div class="relative isolate hover:text-sky-600 justify-center items-center">
+					<LinkedinInBrand aria-label="LinkedIn" class="size-12 w-full" />
+					<h1 class="text-xl leading-none py-2 w-full">LinkedIn</h1>
+				</div>
+			</div>
+		</Card>
+
+		<Card on:click={() => (window.location.href = '/email')}>
+			<h1 class="text-xl mb-4 leading-none">Email</h1>
+			<p class="text-left">Email</p>
+		</Card>
+	</div>
 </div>
+
+<div class="flex justify-center mt-8 gap-8 items-center flex-wrap"></div>
