36c0d3b33ca3b246ba75582c851be7fc44e105aa
Nick Brett <nick.brett1@gmail.com>
2025-02-18 00:22:42 +0000

    Faster display and back to one page

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -2,11 +2,31 @@
 	import Header from '$lib/components/Header.svelte';
 	import Background from '$lib/components/Background.svelte';
 	import Landing from '$lib/components/Landing.svelte';
+	import About from '$lib/components/About.svelte';
+	import Projects from '$lib/components/Projects.svelte';
+
+	function getScrollPercent() {
+		let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+		let clientHeight = document.documentElement.clientHeight;
+
+		return scrollTop / (scrollHeight - clientHeight);
+	}
+	let percentageScroll = $state(0);
 </script>
+
+<svelte:window
+	onscroll={() => {
+		percentageScroll = getScrollPercent();
+		console.log('percentageScroll:', percentageScroll);
+	}}
+/>;
 
 <Background />
 
 <div class="flex flex-col grow h-dvh min-h-max overflow-visible">
 	<Header />
 	<Landing />
+	<About />
+	<Projects />
 </div>
