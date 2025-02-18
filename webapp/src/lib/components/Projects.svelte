0f891658be1962d40d7d009cfa67b0c424092b90
Nick Brett <nick.brett1@gmail.com>
2025-02-18 01:38:20 +0000

    REmove background blur

--- webapp/src/lib/components/Projects.svelte
+++ webapp/src/lib/components/Projects.svelte
@@ -14,7 +14,7 @@
 	} from 'svelte-awesome-icons';
 </script>
 
-<div class="relative left-0 w-full overflow-hidden py-24 backdrop-blur">
+<div class="relative left-0 w-full overflow-hidden py-24">
 	<div class="relative mx-auto max-w-7xl px-4 md:px-6">
 		<div class="relative mx-auto mb-20 text-xl leading-none text-green-400">&rarr; PROJECTS</div>
 	</div>
