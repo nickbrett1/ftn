e07362a20b1d67167c76a15028fda40b4ff7b947
Nick Brett <nick.brett1@gmail.com>
2025-02-18 01:38:20 +0000

    REmove background blur

--- webapp/src/lib/components/About.svelte
+++ webapp/src/lib/components/About.svelte
@@ -1,7 +1,7 @@
 <!-- Inspired by Mario Tiscareno (https://mario.tiscareno.dev/) -->
 
 <!-- spark line instead of right arrow? https://www.chartjs.org/ -->
-<div class="relative left-0 w-full overflow-hidden py-24 backdrop-blur">
+<div class="relative left-0 w-full overflow-hidden py-24">
 	<div class="relative mx-auto max-w-7xl px-4 md:px-6">
 		<div class="relative mx-auto mb-20 text-xl leading-none text-green-400">&rarr; HELLO</div>
 		<p
