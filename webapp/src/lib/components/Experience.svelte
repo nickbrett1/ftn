27329845ed3db66ea01ed960a3bde352fa089d9e
Nick Brett <nick.brett1@gmail.com>
2025-02-23 16:28:58 +0000

    Some tidy up

--- webapp/src/lib/components/Experience.svelte
+++ webapp/src/lib/components/Experience.svelte
@@ -1,3 +1,3 @@
-<div class="py-24">
-	<div class="relative mx-auto mb-20 text-xl leading-none text-green-400">&rarr; EXPERIENCE</div>
+<div>
+	<div class="mb-20 text-xl leading-none text-green-400">&rarr; EXPERIENCE</div>
 </div>
