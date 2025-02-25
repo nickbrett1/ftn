e4c96e652871cacf3c454708e72c4d65649b7c69
Nick Brett <nick.brett1@gmail.com>
2025-02-25 02:21:01 +0000

    Experience!

--- webapp/src/lib/components/Milestone.svelte
+++ webapp/src/lib/components/Milestone.svelte
@@ -1,3 +1,5 @@
+<!-- Inspired by https://mario.tiscareno.dev/ -->
+
 <script>
 	let {
 		heading = '',
@@ -6,42 +8,88 @@
 		keywords = [],
 		icons = [],
 		from = '',
-		to = '',
-		active = false
+		to = ''
 	} = $props();
 </script>
 
 <div
-	class="milestone relative mx-auto flex max-w-3xl items-start justify-center sm:pl-8 sm:before:absolute sm:before:inset-0 sm:before:-left-72 sm:before:h-[1px] sm:before:w-1/2 sm:before:bg-gradient-to-r sm:before:from-white/25 sm:before:to-transparent"
-	class:active
+	class="
+		milestone
+		relative
+		mx-auto
+		flex
+		max-w-3xl
+		items-start
+		justify-center
+		sm:pl-8
+		sm:before:absolute
+		sm:before:inset-0
+		sm:before:-left-72
+		sm:before:h-[1px]
+		sm:before:w-1/2
+		sm:before:bg-gradient-to-r
+		sm:before:from-white/25
+		sm:before:to-transparent"
 >
 	<!-- Date -->
 	<time
-		class="glass hidden h-20 w-48 shrink-0 items-center justify-center rounded-bl border border-b-black/10 border-l-white/10 border-r-black/10 border-t-white/10 bg-white/20 p-2 font-['Roboto_Mono'] text-xs font-semibold text-slate-300 shadow sm:flex"
+		class="
+			h-15
+			w-48
+			shrink-0
+			items-center
+			justify-center
+			rounded-bl
+			border
+		border-b-black/10
+		border-l-white/10
+		border-r-black/10
+		border-t-white/10
+		bg-white/10 p-2
+		text-white
+			tracking-normal
+			font-medium
+			sm:flex"
 	>
 		{from} - {to}
 	</time>
+
 	<!-- Card -->
 	<div
-		class="glass w-full rounded-lg rounded-b-lg rounded-r-lg border border-b-black/10 border-l-white/10 border-r-black/10 border-t-white/10 bg-white/10 p-4 shadow sm:rounded-tl-none sm:p-6"
+		class="
+			w-full
+			rounded-lg
+			rounded-b-lg
+			rounded-r-lg
+			backdrop-blur-sm
+			border
+			border-b-black/10
+			border-l-white/10
+			border-r-black/10
+			border-t-white/10
+			bg-white/5
+			sm:rounded-tl-none
+			sm:p-6"
 	>
 		<div>
-			<h2 class="text-xl font-bold tracking-tight text-slate-300 sm:text-2xl">{heading}</h2>
-			<h3 class="text-sm tracking-tight text-[#fce4b8] drop-shadow lg:text-base">
+			<h2 class="text-xl font-bold tracking-tight text-white sm:text-2xl">{heading}</h2>
+			<h3 class="tracking-tight text-green-400/80 lg:text-lg">
 				{subheading}
 			</h3>
 		</div>
-		<div class="col-span-2 mt-6 flex flex-col gap-4 text-pretty text-xs text-slate-300 sm:text-sm">
+		<div class="mt-6 flex flex-col gap-4 text-pretty text-xs text-white sm:text-sm">
 			{#each body as paragraph}
 				<p class="leading-normal tracking-normal">{paragraph}</p>
 			{/each}
-			<div class="my-6">
-				<ul class="flex flex-wrap gap-1 text-[0.7rem] leading-snug tracking-wide text-white/75">
-					{#each keywords as keyword}
-						<li class="rounded-full bg-black/20 px-2 py-0.5">{keyword}</li>
-					{/each}
-				</ul>
-			</div>
+			{#if keywords.length > 0}
+				<div class="my-6">
+					<ul class="flex flex-wrap gap-1 text-[0.7rem] leading-snug tracking-wide text-white/75">
+						{#each keywords as keyword}
+							<li class="rounded-full bg-white/10 px-2 py-0.5">{keyword}</li>
+						{/each}
+					</ul>
+				</div>
+			{/if}
 		</div>
 		<div class="flex flex-wrap gap-3.5">
 			{#each icons as icon}
