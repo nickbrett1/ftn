a711c9c2b84a520e134b7c7589bc569f8048ce99
Nick Brett <nick.brett1@gmail.com>
2025-02-26 02:09:45 +0000

    Added more content

--- webapp/src/lib/components/Experience.svelte
+++ webapp/src/lib/components/Experience.svelte
@@ -3,17 +3,39 @@
 
 	const work = [
 		{
-			from: 'Feb, 2025',
+			from: 'Feb 2025',
 			to: 'Present',
 			title: 'Senior Vice President: Strategic Initatives',
-			workPlace: 'Data Engineering @ Two Sigma',
+			workplace: 'Data Engineering @ Two Sigma',
 			body: [
 				"I'm currently focused on various strategic initiatives across the Two Sigma Data Engineering department.",
-				'This includes work to build systems that can provide full reproducability of data over time, new data storage architectures to unlock research opportunities and driving complex external partnerships.',
+				'This includes work to build systems that can provide full reproducibility of data over time, new data storage architectures to unlock research opportunities and driving complex external partnerships.',
 				'This work combines a mix of deep technical data expertise, strong product-sense and an ability to collaborate widely across the organization.'
 			],
 			keywords: [],
 			icons: []
+		},
+		{
+			from: 'Dec 2023',
+			to: 'Feb 2025',
+			title: 'Head of Data Platform & Differentiated Data Engineering',
+			workplace: 'Data Engineering @ Two Sigma',
+			body: [
+				'Expanded scope to lead a second seperate cross-functional team "Differentiated Data Engineering" of Data Engineers, Data Scientists and Program Managers.',
+				"The team built and leveraged LLM technology to create differentiated proprietary datasets. Responsibilities encompassed three efforts; building tools that leverage LLM technology that can be used to deliver new sources of alpha, establishing systems and processes that allow the scalable validation and tuning of LLM's with human input, and building datasets with the team's LLM tools that themselves deliver alpha."
+			],
+			keywords: [],
+			icons: []
+		},
+		{
+			from: 'Jan 2023',
+			to: 'Dec 2023',
+			title: 'Head of Data Platform',
+			workplace: 'Data Engineering @ Two Sigma',
+			body: [
+				"Led a team of Data Engineers responsible for Two Sigma's Data Platform, a suite of tools and services that enable Alpha Modelers and other internal customers to discover, share and seamlessly use Two Sigma's vast catalog of vendor supplied, proprietary and derived datasets. Key products include a 'Feature Catalog', that organizes, indexes and permissions economically valuable time-series data (features) in a highly secure environment, and a data lineage system for tracing all data flows across Two Sigma.",
+				'Key achievements included establishing a process and mechanism to provide data lineage insights that delivered use-cases around risk management, support cost reduction and model attribution in a way that did not disrupt modeler workflow or require a rebuild of the existing architecture. Also developing systems to allow data upgrades to occur seamlessly, with alpha models migrated to new data sets with no disruption and minimal effort.'
+			]
 		}
 	];
 </script>
@@ -47,7 +69,7 @@
 				from={w.from}
 				to={w.to}
 				heading={w.title}
-				subheading={w.workPlace}
+				subheading={w.workplace}
 				body={w.body}
 				keywords={w.keywords}
 				icons={w.icons}
