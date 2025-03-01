d4a46fa1af0a18545189a063bf70fad3da504329
Nick Brett <nick.brett1@gmail.com>
2025-03-01 01:49:12 +0000

    Fix header and mack footer consistent

--- webapp/src/lib/components/Footer.svelte
+++ webapp/src/lib/components/Footer.svelte
@@ -9,14 +9,20 @@
 			<li class="hidden flex-1 sm:block"></li>
 
 			<li class="flex">
-				<a href="mailto:nick@fintechnick.com" class="-mt-1 block h-10 w-10 p-1">
+				<a
+					href="mailto:nick@fintechnick.com"
+					class="hover:text-green-300 -mt-1 block h-10 w-10 p-1"
+				>
 					<EnvelopeRegular /></a
 				>
 				<a href="https://github.com/nickbrett1/" class=" -mt-1 block h-10 w-10 p-1">
 					<GithubBrands />
 				</a>
 
-				<a href="https://www.linkedin.com/in/nick-brett/" class=" -mt-1 block h-10 w-10 p-1">
+				<a
+					href="https://www.linkedin.com/in/nick-brett/"
+					class="hover:text-sky-600 -mt-1 block h-10 w-10 p-1"
+				>
 					<LinkedinInBrands />
 				</a>
 			</li>
