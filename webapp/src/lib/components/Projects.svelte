64eed3e0f92d1852706cc708d664dce3ce4b2d70
Nick Brett <nick.brett1@gmail.com>
2025-03-04 01:44:43 +0000

    More content and a Contact section

--- webapp/src/lib/components/Projects.svelte
+++ webapp/src/lib/components/Projects.svelte
@@ -1,13 +1,9 @@
 <script>
 	import Card from '$lib/components/Card.svelte';
 	import HoverFadeCard from '$lib/components/HoverFadeCard.svelte';
-	import GlowButton from '$lib/components/GlowButton.svelte';
 	import {
-		LinkedinInBrands,
-		EnvelopeRegular,
-		EnvelopeOpenRegular,
 		GithubBrands,
-		FileCodeRegular,
+		LightbulbSolid,
 		PenToSquareRegular,
 		ToolboxSolid,
 		PlaneDepartureSolid,
@@ -22,7 +18,7 @@
 		class="grid gap-4 items-stretch auto-rows-fr grid-cols-[repeat(auto-fill,minmax(312px,1fr))] text-white"
 	>
 		<div class="md:p-6 text-pretty md:text-right text-3xl leading-tight tracking-tight">
-			These are some of my personal projects, tools, code, blog and contact information.
+			These are some of my personal projects, tools, code and writings.
 		</div>
 
 		<HoverFadeCard>
@@ -30,7 +26,7 @@
 				<div class="flex flex-col grow">
 					<div class="flex flex-row">
 						<span class="text-xl font-bold leading-none pt-2 grow">Making of fintechnick.com</span>
-						<SitemapSolid class="size-10 " aria-label="Making of FinTechNick" />
+						<SitemapSolid class="size-10" aria-label="Making of FinTechNick" />
 					</div>
 					<p class="grow pt-5">
 						As a 'data person', building a website using modern front-end technologies and patterns
@@ -46,14 +42,17 @@
 		</HoverFadeCard>
 		<HoverFadeCard>
 			{#snippet initial()}
-				<div class="flex flex-col items-center grow">
-					<PlaneDepartureSolid
-						aria-label="Machine Learning for Flight Delays"
-						class="size-20 grow"
-					/>
-					<span class="text-xl leading-none text-center pt-2 grow">
-						Machine Learning for Flight Delays
-					</span>
+				<div class="flex flex-col grow">
+					<div class="flex flex-row">
+						<span class="text-xl font-bold leading-none pt-2 grow"
+							>Machine Learning for Flight Delays</span
+						>
+						<PlaneDepartureSolid class="size-10 " aria-label="Machine Learning for Flight Delays" />
+					</div>
+					<p class="grow pt-5">
+						A machine learning system that uses United States flight data to predict whether a delay
+						is likely for upcoming flights.
+					</p>
 				</div>
 			{/snippet}
 			{#snippet onHover()}
@@ -64,9 +63,14 @@
 		</HoverFadeCard>
 		<HoverFadeCard>
 			{#snippet initial()}
-				<div class="flex flex-col items-center grow">
-					<ToolboxSolid aria-label="Tools" class="size-20 grow" />
-					<span class="text-xl leading-none content-center pt-2 grow">My Tools</span>
+				<div class="flex flex-col grow">
+					<div class="flex flex-row">
+						<span class="text-xl font-bold leading-none pt-2 grow">Product Pieces</span>
+						<LightbulbSolid class="size-10 " aria-label="Tips and Tricks for Product Management" />
+					</div>
+					<p class="grow pt-5">
+						Pieces of insights, and lessons learned, from my learnings on Product Management
+					</p>
 				</div>
 			{/snippet}
 			{#snippet onHover()}
@@ -75,57 +79,50 @@
 				</div>
 			{/snippet}
 		</HoverFadeCard>
-		<HoverFadeCard
-			onclick={() =>
-				window.open('https://github.com/nickbrett1/', '_blank', 'noopener', 'noreferrer')}
-		>
+		<HoverFadeCard>
 			{#snippet initial()}
-				<div class="flex flex-col items-center grow">
-					<GithubBrands aria-label="Github" class="size-20 grow" />
-					<span class="text-xl leading-none content-center pt-2 grow">Github</span>
+				<div class="flex flex-col grow">
+					<div class="flex flex-row">
+						<span class="text-xl font-bold leading-none pt-2 grow">My Tools</span>
+						<ToolboxSolid class="size-10 " aria-label="Tools" />
+					</div>
+					<p class="grow pt-5">
+						Recommendations on the tools I use as an Engineer, Product Manager and Data Person.
+					</p>
 				</div>
 			{/snippet}
 			{#snippet onHover()}
 				<div class="flex flex-col items-center grow">
-					<FileCodeRegular aria-label="Github" class="size-20 grow" />
-					<span class="text-xl leading-none content-center pt-2 grow">Github</span>
+					<span class="text-2xl content-center font-bold grow">Coming Soon!</span>
 				</div>
 			{/snippet}
 		</HoverFadeCard>
+		<Card
+			onclick={() =>
+				window.open('https://github.com/nickbrett1/', '_blank', 'noopener', 'noreferrer')}
+		>
+			<div class="flex flex-col grow">
+				<div class="flex flex-row">
+					<span class="text-xl font-bold leading-none pt-2 grow">My Code</span>
+					<GithubBrands class="size-10" aria-label="Github" />
+				</div>
+				<p class="grow pt-5">Here you'll find all of my code. Feel free to embrace and extend.</p>
+			</div>
+		</Card>
 		<Card
 			onclick={() =>
 				window.open('https://medium.com/the-management-code', '_blank', 'noopener', 'noreferrer')}
 		>
-			<div class="flex flex-col items-center grow">
-				<PenToSquareRegular aria-label="Blog" class="size-20 grow" />
-				<span class="text-xl leading-none content-center pt-2 grow">Blog</span>
+			<div class="flex flex-col grow">
+				<div class="flex flex-row">
+					<span class="text-xl font-bold leading-none pt-2 grow">My Writings</span>
+					<PenToSquareRegular class="size-10" aria-label="Blog" />
+				</div>
+				<p class="grow pt-5">
+					A collection of articles focusing on things I've learned as a Software Engineering
+					Manager.
+				</p>
 			</div>
 		</Card>
-		<Card
-			onclick={() =>
-				window.open('https://www.linkedin.com/in/nick-brett/', '_blank', 'noopener', 'noreferrer')}
-		>
-			<div class="flex flex-col items-center grow hover:text-sky-600">
-				<LinkedinInBrands aria-label="LinkedIn" class="size-20 grow" />
-				<span class="text-xl leading-none content-center pt-2 grow">LinkedIn</span>
-			</div>
-		</Card>
-
-		<HoverFadeCard
-			onclick={() => window.open('mailto:nick@fintechnick.com', '_blank', 'noopener', 'noreferrer')}
-		>
-			{#snippet initial()}
-				<div class="flex flex-col items-center grow">
-					<EnvelopeRegular aria-label="Email" class="size-20 grow" />
-					<span class="text-xl leading-none content-center pt-2 grow">Email</span>
-				</div>
-			{/snippet}
-			{#snippet onHover()}
-				<div class="flex flex-col items-center grow">
-					<EnvelopeOpenRegular aria-label="Email" class="size-20 grow" />
-					<span class="text-xl leading-none content-center pt-2 grow">Email</span>
-				</div>
-			{/snippet}
-		</HoverFadeCard>
 	</div>
 </div>
