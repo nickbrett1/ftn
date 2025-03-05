0b33fa0b419282bd31dfa6cbbf4f53a7fbb2c26a
Nick Brett <nick.brett1@gmail.com>
2025-03-05 02:15:25 +0000

    Prep work for navbar

---
+++ webapp/src/lib/components/Navbar.svelte
@@ -0,0 +1,35 @@
+<script>
+	import DesktopNavigation from './DesktopNavigation.svelte';
+	import MobileNavigation from './MobileNavigation.svelte';
+
+	let { active = 'home' } = $props();
+	const items = ['home'];
+
+	async function setActive() {
+		const { default: mostVisible } = await import('most-visible');
+
+		let currentlyActive = mostVisible(document.querySelectorAll('.section'));
+
+		if (currentlyActive && currentlyActive.id) {
+			active = currentlyActive.id;
+		}
+	}
+</script>
+
+<svelte:window
+	on:scroll={() => {
+		setActive();
+	}}
+/>
+
+<!--
+<div class="fixed top-6 w-full z-40 pointer-events-none">
+	<div class="flex flex-1 justify-end md:justify-center">
+		<DesktopNavigation {items} bind:active class="pointer-events-auto hidden md:block" />
+	</div>
+</div>
+
+<div class="fixed bottom-6 right-0 z-40">
+	<MobileNavigation {items} bind:active class="pointer-events-auto md:hidden mt-10" />
+</div>
+-->
