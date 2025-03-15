08c71acc68f5790424bd8efe1426b2d9ef28f1e6
Nick Brett <nick.brett1@gmail.com>
2025-03-15 01:08:12 +0000

    Fix bug

--- webapp/src/lib/components/MobileNavigationItem.svelte
+++ webapp/src/lib/components/MobileNavigationItem.svelte
@@ -1,31 +1,18 @@
 <script>
+	import { scrollIntoView } from '$lib/utils/utils';
+
 	let { current = 'home', active = 'home', hide = () => {} } = $props();
-
-	function scrollIntoView({ target }) {
-		const el = document.querySelector(target.getAttribute('href'));
-		if (!el) return;
-		el.scrollIntoView({
-			behavior: 'smooth'
-		});
-	}
-
-	function once(fn) {
-		return function (event) {
-			if (fn) fn.call(this, event);
-			fn = null;
-		};
-	}
 </script>
 
 <li>
 	<a
 		href="#{current}"
 		class="block py-2 {active == current ? 'text-green-400' : ''}"
-		onclick={once((e) => {
+		onclick={(e) => {
 			e.preventDefault();
-			hide();
 			scrollIntoView(e);
-		})}
+			hide();
+		}}
 	>
 		{current}
 	</a>
