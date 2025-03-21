8f3a1b2897a188c44cf3d6dcff51939e45c068cd
Nick Brett <nick.brett1@gmail.com>
2025-03-21 01:31:47 +0000

    So many logos!

---
+++ webapp/src/lib/icons/Bloomberg.svelte
@@ -0,0 +1,6 @@
+<script>
+	import icon from '$lib/images/bloomberg-icon.jpeg?as=run';
+	import Img from '@zerodevx/svelte-img';
+</script>
+
+<Img src={icon} alt="Bloomberg" class="h-full " />
---
+++ webapp/src/lib/icons/CollaborativeGain.svelte
@@ -0,0 +1,6 @@
+<script>
+	import icon from '$lib/images/collaborative-gain-logo.png?as=run:1';
+	import Img from '@zerodevx/svelte-img';
+</script>
+
+<Img src={icon} alt="Collaborative Gain" class="h-full " />
