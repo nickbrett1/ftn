472394cfa3ce8553333daddaf2571dfbc1aa2bb6
Nick Brett <nick.brett1@gmail.com>
2025-03-23 02:29:07 +0000

    TwoSigma icon clickable

---
+++ webapp/src/lib/components/CompanyLogo.svelte
@@ -0,0 +1,29 @@
+<script>
+	import tippy from 'tippy.js';
+	import 'tippy.js/dist/tippy.css';
+	import { onMount } from 'svelte';
+	import { nanoid } from 'nanoid';
+
+	let { tooltip = '', link = '', icon = null } = $props();
+
+	let id = nanoid();
+
+	onMount(() => {
+		tippy('#' + id, {
+			content: { tooltip }
+		});
+	});
+</script>
+
+<button
+	class="size-full cursor-pointer"
+	type="button"
+	aria-label={tooltip}
+	onclick={() => {
+		window.location = link;
+	}}
+>
+	{#if icon}
+		{@render icon()}
+	{/if}
+</button>
