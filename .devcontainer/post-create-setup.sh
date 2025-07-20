e40c3e2f0ee6d74ef203c86e026e47bb00867f78
Nick Brett <nick.brett1@gmail.com>
2025-07-20 13:52:34 -0400

    Improving container

--- .devcontainer/post-create-setup.sh
+++ .devcontainer/post-create-setup.sh
@@ -42,17 +42,17 @@ fi
 echo "INFO: Configuring git safe directory..."
 git config --global --add safe.directory /workspaces/ftn
 
-WEBAPP_DIR="/workspaces/ftn/webapp"
-if [ -d "$WEBAPP_DIR" ]; then
+# WEBAPP_DIR="/workspaces/ftn/webapp"
+# if [ -d "$WEBAPP_DIR" ]; then
 
-    echo "INFO: Webapp directory found at $WEBAPP_DIR. Checking Wrangler login status..."
-    ( # Start a subshell to localize the cd
-        cd "$WEBAPP_DIR"
-        npx wrangler login --browser=false --callback-host=0.0.0.0 --callback-port=8976 | stdbuf -oL sed 's/0\.0\.0\.0/localhost/g'
-        echo "INFO: Wrangler login process initiated."
-    ) # End subshell
-else
-    echo "INFO: Webapp directory not found at $WEBAPP_DIR, skipping Wrangler login."
-fi
+#   echo "INFO: Webapp directory found at $WEBAPP_DIR. Checking Wrangler login status..."
+#   ( # Start a subshell to localize the cd
+#       cd "$WEBAPP_DIR"
+#       npx wrangler login --browser=false --callback-host=0.0.0.0 --callback-port=8976 | stdbuf -oL sed 's/0\.0\.0\.0/localhost/g'
+#       echo "INFO: Wrangler login process initiated."
+#   ) # End subshell
+# else
+#   echo "INFO: Webapp directory not found at $WEBAPP_DIR, skipping Wrangler login."
+# fi
 
 echo "INFO: Custom container setup script finished."
---
+++ .devcontainer/post-start-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run wrangler login after container ports are available
+WEBAPP_DIR="/workspaces/ftn/webapp"
+if [ -d "$WEBAPP_DIR" ]; then
+	echo "INFO: Webapp directory found at $WEBAPP_DIR. Checking Wrangler login status..."
+	( # Start a subshell to localize the cd
+    cd "$WEBAPP_DIR"
+    npx wrangler login --browser=false --callback-host=0.0.0.0 --callback-port=8976 | stdbuf -oL sed 's/0\.0\.0\.0/localhost/g'
+    echo "INFO: Wrangler login process initiated."
+	) # End subshell
+else
+  echo "INFO: Webapp directory not found at $WEBAPP_DIR, skipping Wrangler login."
+fi
