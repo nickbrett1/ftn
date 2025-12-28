dd8cc494d07da82a565f921480234bec837b0107
Nick Brett <nick.brett1@gmail.com>
2025-12-28 19:42:21 +0000

    fix(genproj): update generated dependencies to match ftn project versions

--- webapp/src/lib/utils/file-generator.js
+++ webapp/src/lib/utils/file-generator.js
@@ -461,13 +461,13 @@ function generatePackageJson(templateEngine, context) {
 		scripts =
 			',\n    "dev": "vite dev",\n    "build": "vite build",\n    "preview": "vite preview",\n    "check": "svelte-kit sync && svelte-check",\n    "check:watch": "svelte-kit sync && svelte-check --watch"';
 		devDependencies +=
-			'"@sveltejs/kit": "^2.16.0",\n    "@sveltejs/vite-plugin-svelte": "^5.0.0",\n    "svelte": "^5.0.0",\n    "svelte-check": "^4.0.0",\n    "typescript": "^5.0.0",\n    "vite": "^6.0.0"';
+			'"@sveltejs/kit": "^2.49.2",\n    "@sveltejs/vite-plugin-svelte": "^6.2.1",\n    "svelte": "^5.46.1",\n    "svelte-check": "^4.1.1",\n    "typescript": "^5.7.2",\n    "vite": "^7.3.0"';
 
 		if (hasWrangler) {
 			scripts += ',\n    "deploy": "wrangler deploy"';
-			devDependencies += ',\n    "@sveltejs/adapter-cloudflare": "^5.0.3"';
+			devDependencies += ',\n    "@sveltejs/adapter-cloudflare": "^7.2.4"';
 			// Wrangler is also needed as dev dep
-			devDependencies += ',\n    "wrangler": "^3.57.0"';
+			devDependencies += ',\n    "wrangler": "^4.56.0"';
 		} else {
 			devDependencies += ',\n    "@sveltejs/adapter-auto": "^3.0.0"';
 		}
