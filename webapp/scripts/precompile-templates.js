f028f75f52ae4326b50824b2ba14d6685fb4fb38
Nick Brett <nick.brett1@gmail.com>
2026-07-11 01:11:21 +0000

    fixes

--- webapp/scripts/precompile-templates.js
+++ webapp/scripts/precompile-templates.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 import Handlebars from 'handlebars';
 
 const templatesDir = './src/lib/templates';
